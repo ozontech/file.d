8903d5c496b6f362cdf9dfd614167e015777a40f
vvitkovskiy <vvitkovskiy@ozon.ru>
2020-01-21 17:05:55 +0300

    Documentation example with insane-doc

--- plugin/input/file/file.go
+++ plugin/input/file/file.go
@@ -8,44 +8,24 @@ import (
 	"gitlab.ozon.ru/sre/file-d/pipeline"
 )
 
-const (
-	defaultWorkersCount    = 16
-	defaultReadBufferSize  = 128 * 1024
-	defaultMaxFiles        = 16384
-	defaultPersistInterval = time.Second
-
-	defaultPersistenceMode = "timer"
-	defaultOffsetsOp       = "continue"
-
-	persistenceModeSync  persistenceMode = 0
-	persistenceModeAsync persistenceMode = 1
-
-	offsetsOpContinue offsetsOp = 0
-	offsetsOpReset    offsetsOp = 1
-	offsetsOpTail     offsetsOp = 2
-)
-
-type (
-	persistenceMode int
-	offsetsOp       int
-)
-
-type Config struct {
-	WatchingDir     string        `json:"watching_dir"`
-	FilenamePattern string        `json:"filename_pattern"`
-	OffsetsFile     string        `json:"offsets_file"`
-	PersistenceMode string        `json:"persistence_mode"`
-	PersistInterval time.Duration `json:"persist_interval"`
-	ReadBufferSize  int           `json:"read_buffer_size"`
-	MaxFiles        int           `json:"max_files"`
-	OffsetsOp       string        `json:"offsets_op"` // continue|tail|reset
-	WorkersCount    int           `json:"workers_count"`
-
-	offsetsTmpFilename string
-	persistenceMode    persistenceMode
-	offsetsOp          offsetsOp
-}
-
+/* {! comment @description
+Plugin is watching for files in the provided directory and reads them line by line.
+Each line should contain only one event. It also correctly handles rotations (rename/truncate) and symlinks.
+From time to time it instantly releases and reopens descriptors of completely processed files.
+Such behaviour allows files to be deleted by third party software even though `file-d` is still working(in this case reopen will fail).
+Watcher trying to use file system events to watch for files.
+But events isn't work with symlinks, so watcher also manually rescans directory by some interval.
+Config example:
+```
+pipelines:
+  docker:
+	  type: file
+	  persistence_mode: async
+	  watching_dir: /var/lib/docker/containers
+	  filename_pattern: "*-json.log"
+	  offsets_file: /data/offsets.yaml
+```
+!} */
 type Plugin struct {
 	config *Config
 	params *pipeline.InputPluginParams
@@ -53,6 +33,84 @@ type Plugin struct {
 	workers     []*worker
 	jobProvider *jobProvider
 }
+
+// {! options #2 #6
+type persistenceMode int
+
+const (
+	persistenceModeSync  persistenceMode = 0 // sync *! saves offsets file with event commitment. It's very slow, but excludes possibility of events duplication in extreme situations like power loss.
+	persistenceModeAsync persistenceMode = 1 // async *! saves offsets file by timer using `persist_interval`. Saving is skipped if offsets haven't been changed. Suitable in most cases, guarantees at least once delivery and makes almost no overhead.
+) // !}
+
+// {! options #2 #6
+type offsetsOp int
+
+const (
+	offsetsOpContinue offsetsOp = 0 // continue *! set offset as saved in offsets file.
+	offsetsOpReset    offsetsOp = 1 // reset *! set offset to the beginning of the file.
+	offsetsOpTail     offsetsOp = 2 // tail *! set offset to the end of the file.
+) // !}
+
+type Config struct {
+	// {! params @config /json:\"(.+)\"`/
+	WatchingDir     string `json:"watching_dir"`     // *! `string` `required` <br> <br> Directory to watch for files.
+	OffsetsFile     string `json:"offsets_file"`     // *! `string` `required` <br> <br> File name into which offsets will be saving. It's simply yaml file. You can read and manually modify it. Offsets are loaded only on initialization.
+	FilenamePattern string `json:"filename_pattern"` // *! `string` `default=[! @defaults.defaultFilenamePattern !]` <br> <br> Files which doesn't match this pattern will be ignored. Check out https://golang.org/pkg/path/filepath/#Glob for details.
+	/*
+		>! `string="[! @persistenceMode.variants !]"` `default=[! @defaults.defaultPersistenceMode !]`
+		>!
+		>! Defines how to save offsets file:
+		>! [! @persistenceMode.legend !]
+		>!
+		>! Save means doing three stages:
+		>! * Write temporary file with all offsets
+		>! * Call `fsync()` on it
+		>! * Rename temporary file to original
+		>!
+	*/
+	PersistenceMode string `json:"persistence_mode"` // *!
+
+	PersistInterval time.Duration `json:"persist_interval"` // *! `default=[! @defaults.defaultPersistInterval !]` <br> <br> Offsets save interval. Used only if `persistence_mode` is `async`.
+	ReadBufferSize  int           `json:"read_buffer_size"` // *! `default=[! @defaults.defaultReadBufferSize !]` <br> <br> Size of buffer to use for file reading. Each worker use own buffer, so memory consumption will be `read_buffer_size*workers_count`.
+	MaxFiles        int           `json:"max_files"`        // *! `default=[! @defaults.defaultMaxFiles !]` <br> <br> Max amount of opened files. If the limit is exceeded `file-d` will exit with fatal. Also check your file descriptors limit: `ulimit -n`.
+
+	/*
+		>! `string="[! @offsetsOp.variants !]"` default=`[! @defaults.defaultOffsetsOp !]`
+		>!
+		>! Offset operation which will be preformed when adding file as a job:
+		>! [! @offsetsOp.legend !]
+		>! > It is only used on initial scan of `watching_dir`. Files which will be catched up later during work always use `reset` operation.
+	*/
+	OffsetsOp string `json:"offsets_op"` // *!
+
+	/*
+		>! `number` default=`[! @defaults.defaultWorkersCount !]`
+		>!
+		>! How much workers will be instantiated. Each worker:
+		>! * Read files (I/O bound)
+		>! * Decode events (CPU bound)
+		>! > It's recommended to set it to 4x-8x of CPU cores.
+	*/
+	WorkersCount int `json:"workers_count"` // *!
+	// !}
+
+	offsetsTmpFilename string
+	persistenceMode    persistenceMode
+	offsetsOp          offsetsOp
+}
+
+// {! values @defaults #1 #3
+const (
+	defaultWorkersCount    = 16          // *!
+	defaultReadBufferSize  = 131072      // *!
+	defaultMaxFiles        = 16384       // *!
+	defaultPersistInterval = time.Second // *!
+	defaultPersistenceMode = "async"     // *!
+	defaultOffsetsOp       = "continue"  // *!
+	defaultFilenamePattern = "*"         // *!
+)
+
+// !}
 
 func init() {
 	fd.DefaultPluginRegistry.RegisterInput(&pipeline.PluginStaticInfo{
@@ -110,7 +168,7 @@ func (p *Plugin) Start(config pipeline.AnyConfig, params *pipeline.InputPluginPa
 	}
 
 	if p.config.FilenamePattern == "" {
-		p.config.FilenamePattern = "*"
+		p.config.FilenamePattern = defaultFilenamePattern
 	}
 
 	if p.config.WorkersCount == 0 {
