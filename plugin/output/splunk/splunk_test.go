169b712a7ef40ad18f308f011ba0a63d85122809
andy <andy@desktop>
2021-12-30 22:34:27 +0300

    PR fixes

--- plugin/output/splunk/splunk_test.go
+++ plugin/output/splunk/splunk_test.go
@@ -2,6 +2,7 @@ package splunk
 
 import (
 	"github.com/ozonru/file.d/pipeline"
+	"github.com/stretchr/testify/assert"
 	insaneJSON "github.com/vitkovskii/insane-json"
 	"go.uber.org/zap"
 	"io/ioutil"
@@ -11,56 +12,60 @@ import (
 )
 
 func TestEventKey(t *testing.T) {
-	testsCases := map[string]string{
-		`{"msg":"AAAA","some_field":"BBBB"}`:         `{"event":{"msg":"AAAA"},"some_field":"BBBB"}`,
-		`{"msg":"AAAA","some_field":{"boo": "bar"}}`: `{"event":{"msg":"AAAA"},"some_field":{"boo":"bar"}}`,
-	}
-
-	for k, v := range testsCases {
-		if resp := testOne(k, t); resp != v {
-			println(k)
-			println(v)
-			t.Fatal(resp)
-		}
-	}
-}
-
-func testOne(in string, t *testing.T) string {
-	input, err := insaneJSON.DecodeBytes([]byte(in))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var response []byte
-	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		var err error
-		response, err = ioutil.ReadAll(req.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
-		res.WriteHeader(http.StatusOK)
-		res.Write([]byte(`{"code":0}`))
-	}))
-
-	defer func() { testServer.Close() }()
-
-	logger := zap.NewExample().Sugar()
-	plugin := Plugin{
-		config: &Config{
-			EventKey: "msg",
-			Endpoint: testServer.URL,
+	suites := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			`basic case`,
+			`{"msg":"AAAA","some_field":"BBBB"}`,
+			`{"event":{"msg":"AAAA"},"some_field":"BBBB"}`,
 		},
-		logger: logger,
+		{
+			`composite field`,
+			`{"msg":"AAAA","some_field":{"boo": "bar"}}`,
+			`{"event":{"msg":"AAAA"},"some_field":{"boo":"bar"}}`,
+		},
 	}
 
-	batch := pipeline.Batch{
-		Events: []*pipeline.Event{&pipeline.Event{
-			Root: input,
-		}},
+	for _, testCase := range suites {
+		t.Run(testCase.name, func(t *testing.T) {
+			input, err := insaneJSON.DecodeBytes([]byte(testCase.input))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer insaneJSON.Release(input)
+
+			var response []byte
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				response, err = ioutil.ReadAll(req.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				res.WriteHeader(http.StatusOK)
+				res.Write([]byte(`{"code":0}`))
+			}))
+			defer testServer.Close()
+
+			plugin := Plugin{
+				config: &Config{
+					EventKey: "msg",
+					Endpoint: testServer.URL,
+				},
+				logger: zap.NewExample().Sugar(),
+			}
+
+			batch := pipeline.Batch{
+				Events: []*pipeline.Event{{
+					Root: input,
+				}},
+			}
+
+			data := pipeline.WorkerData(nil)
+			plugin.out(&data, &batch)
+
+			assert.Equal(t, testCase.expected, string(response))
+		})
 	}
-
-	data := pipeline.WorkerData(nil)
-	plugin.out(&data, &batch)
-
-	return string(response)
 }
