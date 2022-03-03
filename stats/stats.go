3c1168453934261d8667b7a6fa6274fdbd6f4db2
Sandu Kiritsa <skiritsa@ozon.ru>
2022-03-03 12:17:51 +0300

    Panic when unitialized stats module called. Small tweaks to the Dockerfile.

--- stats/stats.go
+++ stats/stats.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"github.com/ozontech/file.d/logger"
 	"github.com/ozontech/file.d/pipeline"
 	prom "github.com/prometheus/client_golang/prometheus"
 )
@@ -23,12 +24,19 @@ type stats struct {
 	allMetrics map[key]AnyMetric
 }
 
+const (
+	subsystemName  = "stats"
+	unknownCounter = "unknown_counter"
+)
+
 var statsGlobal *stats
 
 func InitStats() {
 	statsGlobal = &stats{
 		allMetrics: make(map[key]AnyMetric),
 	}
+
+	statsGlobal.registerUnknown()
 }
 
 func RegisterCounter(metricDesc *MetricDesc) {
@@ -40,19 +48,39 @@ func RegisterCounter(metricDesc *MetricDesc) {
 	})
 
 	keyInternal := key{pipeline.PromNamespace, metricDesc.Subsystem, metricDesc.Name}
-	statsGlobal.registerMetric(keyInternal, maskPromCounter)
+	registerMetric(keyInternal, maskPromCounter)
 }
 
 func GetCounter(subsystem, metricName string) prom.Counter {
-	return statsGlobal.allMetrics[getKey(subsystem, metricName)].(prom.Counter)
+	if statsGlobal == nil {
+		logger.Panicf("stats package uninitialized")
+	}
+
+	if val, ok := statsGlobal.allMetrics[getKey(subsystem, metricName)]; ok {
+		return val.(prom.Counter)
+	}
+
+	return statsGlobal.allMetrics[getKey(subsystemName, unknownCounter)].(prom.Counter)
 }
 
 func getKey(subsystem, metricName string) key {
 	return key{pipeline.PromNamespace, subsystem, metricName}
 }
 
-func (s *stats) registerMetric(k key, metric prom.Collector) {
-	s.allMetrics[k] = metric
+func registerMetric(k key, metric prom.Collector) {
+	if statsGlobal == nil {
+		logger.Panicf("stats package uninitialized")
+	}
+
+	statsGlobal.allMetrics[k] = metric
 	prom.DefaultRegisterer.Unregister(metric)
 	prom.DefaultRegisterer.MustRegister(metric)
+}
+
+func (s *stats) registerUnknown() {
+	RegisterCounter(&MetricDesc{
+		Subsystem: subsystemName,
+		Name:      unknownCounter,
+		Help:      "Counter for non-existent metrics",
+	})
 }
