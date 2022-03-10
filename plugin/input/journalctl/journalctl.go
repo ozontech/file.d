7ffbc28a884782d40aef253ddc1ddda40ac31a70
Sandu Kiritsa <skiritsa@ozon.ru>
2022-03-10 14:54:59 +0300

    Move journalctl constants to separate file

--- plugin/input/journalctl/journalctl.go
+++ plugin/input/journalctl/journalctl.go
@@ -19,14 +19,6 @@ type Plugin struct {
 	offInfo *offsetInfo
 }
 
-const (
-	subsystemName = "input_journalctl"
-
-	offsetErrors         = "offset_errors"
-	readerErrors         = "reader_errors"
-	journalCtlStopErrors = "journalctl_stop_errors"
-)
-
 type Config struct {
 	//! config-params
 	//^ config-params
