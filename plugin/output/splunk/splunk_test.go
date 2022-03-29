337e84ecc4c243c5083511ad771db297d18aac7c
Andrey Medvedev <andreymedvedev@ozon.ru>
2022-03-29 19:06:47 +0300

    add splunk test

---
+++ plugin/output/splunk/splunk_test.go
@@ -0,0 +1,70 @@
+package splunk
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ozontech/file.d/pipeline"
+	"github.com/stretchr/testify/assert"
+	insaneJSON "github.com/vitkovskii/insane-json"
+	"go.uber.org/zap"
+)
+
+func TestSplunk(t *testing.T) {
+	suites := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			`basic case`,
+			`{"msg":"AAAA","some_field":"BBBB"}`,
+			`{"event":{"msg":"AAAA","some_field":"BBBB"}}`,
+		},
+	}
+
+	for _, testCase := range suites {
+		t.Run(testCase.name, func(t *testing.T) {
+			input, err := insaneJSON.DecodeBytes([]byte(testCase.input))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer insaneJSON.Release(input)
+
+			var response []byte
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				response, err = ioutil.ReadAll(req.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				res.WriteHeader(http.StatusOK)
+				_, _ = res.Write([]byte(`{"code":0}`))
+			}))
+			defer testServer.Close()
+
+			plugin := Plugin{
+				config: &Config{
+					Endpoint: testServer.URL,
+				},
+				logger: zap.NewExample().Sugar(),
+			}
+
+			batch := pipeline.Batch{
+				Events: []*pipeline.Event{
+					{
+						Root: input,
+					},
+					{
+						Root: input,
+					}},
+			}
+
+			data := pipeline.WorkerData(nil)
+			plugin.out(&data, &batch)
+
+			assert.Equal(t, testCase.expected+testCase.expected, string(response))
+		})
+	}
+}
