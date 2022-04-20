a1740e5b9bbba47226aef405b24611b3b7643b42
Sandu Kiritsa <skiritsa@ozon.ru>
2022-04-20 17:02:19 +0300

    Add test for DeltaWrapper.

---
+++ pipeline/delta_wrapper_test.go
@@ -0,0 +1,21 @@
+package pipeline
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+)
+
+func TestDeltaWrapper(t *testing.T) {
+	dw := newDeltaWrapper()
+	assert.Equal(t, int64(0), dw.get())
+
+	initial := int64(0)
+
+	for i := 0; i <= 1000000; i++ {
+		delta := rand.Int63n(5000)
+		initial += delta
+		assert.Equal(t, delta, int64(dw.updateValue(initial)))
+		assert.Equal(t, initial, dw.get())
+	}
+}
