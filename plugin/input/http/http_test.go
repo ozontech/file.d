246641d223e32db96a54eb3422d4c47eb6045aae
Vadim Alekseev <vadimaleksv@gmail.com>
2022-06-06 21:19:05 +0300

    Move PEM parse logic to the new package

--- plugin/input/http/http_test.go
+++ plugin/input/http/http_test.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/ozontech/file.d/cfg"
 	"github.com/ozontech/file.d/pipeline"
@@ -240,80 +239,4 @@ func BenchmarkHttpInputJson(b *testing.B) {
 
 		p.Stop()
 	}
-}
-
-func TestPlugin_fetchTLSConfig(t *testing.T) {
-	var (
-		fakeCertPath = "/tmp/localhost.crt"
-		fakeCert     = `
------BEGIN CERTIFICATE-----
-MIIDhDCCAmygAwIBAgIhANG4j9rHsCqX42dYqniv7bUWIakPRii44fVDlAFslA1N
-MA0GCSqGSIb3DQEBBQUAMFYxCTAHBgNVBAYTADEJMAcGA1UECgwAMQkwBwYDVQQL
-DAAxEDAOBgNVBAMMB296b24ucnUxDzANBgkqhkiG9w0BCQEWADEQMA4GA1UEAwwH
-b3pvbi5ydTAeFw0yMjA2MDYxNjAyMDlaFw0zMjA2MDYxNjAyMDlaMEQxCTAHBgNV
-BAYTADEJMAcGA1UECgwAMQkwBwYDVQQLDAAxEDAOBgNVBAMMB296b24ucnUxDzAN
-BgkqhkiG9w0BCQEWADCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMGr
-dnRoQe4Dn+8GDx2jviswx0KFo7EGz9T+RtFQblTpgGcSWSx0irRk5ce/kDveDqYh
-4ZgNBn4XGnDcogWJAGcq4XymCQq5jBHJ0q7n2GxZ+Tx5vlmyaDNG5zv1lLjgSl0P
-22ClM8NM7hCnE3uZz90VVyQbZ9FFKFW2WX+3uJHJnwMtW+w/n8DnQOBlIAjyCRko
-sRci9/dgaWFmx+Yscs1UPtG7pgEOO+6hiyFNgZEVGbgZv82jR4g8zr6SxcIT8ZLT
-A4oOj+7GFSf8q6yNgp8bDNxbBS3rS2Vv6IrIjXsVfbilWGM3WkAVaOVD6OFJ65wc
-AiggDm9oTFz0H2OOAZ0CAwEAAaNPME0wHQYDVR0OBBYEFHP9uHY/St1je+k0cZ/U
-0ZO2KWJ8MB8GA1UdIwQYMBaAFHP9uHY/St1je+k0cZ/U0ZO2KWJ8MAsGA1UdEQQE
-MAKCADANBgkqhkiG9w0BAQUFAAOCAQEANr3j2VVGCa87fHgJzs3k7IBq0VnXx1C1
-tuWsJ96z0MPHcOD2g8OU2ZANBSGn/PhS71fqvRefr4wUKDLt947/Ycriai84lMv8
-tWO5KLrHDGG8OuMy5orHVt2okcPFVsVmzJBxIv6fRtrzr152FvvRZegrsgnYloZL
-rJdkHHl+9kHS02Nmf3+F6ANAg/jgnr0huJEt1xwB0YtKHTj346ksBP3OOyjolze9
-J6xWsrb/fzFceDYNmIW5oyDm9j6ZxmgRa0turlo/pCp4gEojlyTGhg0HzxoPJmky
-BQQbQaVBlv7XzP9xayJxOOtKiSXjv6OMFP1XFHZPzvx5/3VbVPrKsQ==
------END CERTIFICATE-----`
-		fakePrivateKey = `
------BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAwat2dGhB7gOf7wYPHaO+KzDHQoWjsQbP1P5G0VBuVOmAZxJZ
-LHSKtGTlx7+QO94OpiHhmA0GfhcacNyiBYkAZyrhfKYJCrmMEcnSrufYbFn5PHm+
-WbJoM0bnO/WUuOBKXQ/bYKUzw0zuEKcTe5nP3RVXJBtn0UUoVbZZf7e4kcmfAy1b
-7D+fwOdA4GUgCPIJGSixFyL392BpYWbH5ixyzVQ+0bumAQ477qGLIU2BkRUZuBm/
-zaNHiDzOvpLFwhPxktMDig6P7sYVJ/yrrI2CnxsM3FsFLetLZW/oisiNexV9uKVY
-YzdaQBVo5UPo4UnrnBwCKCAOb2hMXPQfY44BnQIDAQABAoIBAB62CnA2MjJEp+F2
-9AGuvBLMRuTRHV6OpnlvoxpBJ2XWq1Js8fAfQPmPQHLW+U/NDESi/cunUR7AmiRI
-kHbRPVuezKgAClIFj2UovHyY7lRsRh4lbh56MJTlCvkSnfVdN92fDJo8SEORmPTS
-EcBZaUF1R17F68YfMeC5rGKY5y9S432LrsbMapfZhBOBLlSec4Xb1VRAy0OfLzka
-2Gt9B4P4sHzzO6w+ODL/cedfZt687iCfaCzW3QwxSxnelFEb2GOD0KGhuIgN1Nkh
-N+DsAMmjLk5cYwlRKbwv7zuohcSi7MAu/ziKcaKPTGi4kB1cWhVNIuVmz0Ow6lVM
-b1gGfAECgYEA4+E8IEbMmgIpfMmgmYhY1DNcSxp7dqJBwx9i/lbF+OMWo7HFf9zS
-3AlyO3uPXZwgY7DtN4wCYZNFaZxZ+ZiUbMUd/tFKJiFSgx/H1RzKZnP9L60s00t5
-Zx/bIkKv4SnOkBEmYr0x3y6cIvdCRJpK3gDRNdTsiQ2wROp6EhmuV50CgYEA2ZGG
-thkfL6oFqN5pu070qoBAdxSIerVQPWMugChP03G0omNggwvJQ1DBFIhJSYGzyy/E
-vtZczX6ra1AF07H5Xd/R3Dnl2UkQYAo/0wX0dRczl4InB3YUKUkGjpiagQIs3obg
-I8LGzB+gigcPG5cBbn9Q2mCVlN0W7X2qeiffMgECgYA9eNHuZwrkdLbaWc2//P7x
-Z1V7UQ6DJywURdA/igrno+LEj70WS+x5vPaawy8ecnJuUhj7xgISblboMWw4H4fS
-e64mwjB9brnCc31FqPmUf6+J/+46pX5/aiGD8XdehxeY13eCZUUhzoICEK9IsFKH
-5rKJEgIoyo+FV7dMUK8uJQKBgQCL/eh5GTcI21Si8OGFhWtGnhlcxdh/ZFUJ+tx7
-1/RtMDezWVBLYfURnE9wX7UQWCWQ8t6ckJ/MFdpExYvKSDUVIyQMmTB9HFcuBMpG
-hQljohcvQK7OTTrxyawvap/XrMekM5LbT8PMqfkJdztQXFyudbtXmFgHHi9Xhsl/
-qlIiAQKBgA9DhReh6kSqhP0ldvw9f5z2PrVJqGOaCgkRQ/dUuDhbvlgyXI0gS/Rj
-cG6ys3uVh5sYM7eVdZDKrKg45cQddUkbZm07cymaJ0WSSVBvWcUPPZ3aBlMWY4Vj
-rO3s7sbqs3cMwC5g9LjPsfieoZt9AppSrJeY+l74yNa/afqTscun
------END RSA PRIVATE KEY-----`
-	)
-
-	fakeReadFile := func(name string) ([]byte, error) {
-		if name == fakeCertPath {
-			return []byte(fakeCert), nil
-		}
-		panic("invalid file name")
-	}
-
-	input := &Plugin{
-		config: &Config{
-			CACert:     fakeCertPath,   // path to the PEM encoded file
-			PrivateKey: fakePrivateKey, // PEM encoded file
-		},
-	}
-
-	tlsConfig, err := input.fetchTLSConfig(fakeReadFile)
-
-	require.NoError(t, err)
-	require.NotNil(t, tlsConfig)
-	require.Len(t, tlsConfig.Certificates, 1)
 }
