895f29d7cbb4c41799d856fed57760ea7406d0d6
Vadim Alekseev <vadimaleksv@gmail.com>
2022-07-22 00:33:49 +0300

    Support batch size and batch quantity in the ES plugin

--- plugin/output/kafka/kafka.go
+++ plugin/output/kafka/kafka.go
@@ -112,6 +112,7 @@ func (p *Plugin) Start(config pipeline.AnyConfig, params *pipeline.OutputPluginP
 		p.controller,
 		p.config.WorkersCount_,
 		p.config.BatchSize_,
+		0,
 		p.config.BatchFlushTimeout_,
 		0,
 	)
