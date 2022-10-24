769f178c862a983c20f360573e0faa883f61caa6
Dmitry Smolonogov <dsmolonogov@ozon.ru>
2022-10-24 21:02:56 +0300

    add test for http-file configuration

--- cmd/file.d_e2e_new_test.go
+++ cmd/file.d_e2e_new_test.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	http2 "net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -23,35 +25,58 @@ import (
 /*
 General interface for e2e tests
 AddConfigSettings - function that preparing а config for your test
-Send - function that send message in pipeline
+Send - function that send message in pipeline and waits for the end of processing
 Validate - function that validate result of the work
 */
 
 type e2eTest interface {
-	AddConfigSettings(conf *cfg.Config, t *testing.T)
+	AddConfigSettings(t *testing.T, conf *cfg.Config, pipeName string)
 	Send(t *testing.T)
 	Validate(t *testing.T)
 }
 
-func startForTest(e e2eTest, configPath string, t *testing.T) {
+func startForTest(t *testing.T, e e2eTest, configPath, pipeName string) *fd.FileD {
 	conf := cfg.NewConfigFromFile(configPath)
-	e.AddConfigSettings(conf, t)
-	fileD = fd.New(conf, ":9000")
-	fileD.Start()
+	e.AddConfigSettings(t, conf, pipeName)
+	filed := fd.New(conf, "off")
+	filed.Start()
+	return filed
 }
 
 func TestE2EStabilityWorkCase(t *testing.T) {
-	ff := &fileFile{
-		outputNamePattern: "/file-d.log",
-		filePattern:       "pod_ns_container-*",
-		count:             10,
-		lines:             500,
-		retTime:           "1s",
+	testsList := []struct {
+		e2eTest
+		cfgPath  string
+		pipeName string
+	}{
+		{
+			e2eTest: &fileFile{
+				outputNamePattern: "/file-d.log",
+				filePattern:       "pod_ns_container-*",
+				count:             10,
+				lines:             500,
+				retTime:           "1s",
+			},
+			cfgPath:  "./../testdata/config/e2e_cfg/file_file.yaml",
+			pipeName: "test_file_file",
+		},
+		{
+			e2eTest: &httpFile{
+				outputNamePattern: "/file-d.log",
+				count:             10,
+				lines:             500,
+				retTime:           "1s",
+			},
+			cfgPath:  "./../testdata/config/e2e_cfg/http_file.yaml",
+			pipeName: "test_http_file",
+		},
 	}
-	cfgPath := "./../testdata/config/e2e_cfg/file_file.yaml"
-	startForTest(ff, cfgPath, t)
-	ff.Send(t)
-	ff.Validate(t)
+
+	for _, test := range testsList {
+		startForTest(t, test.e2eTest, test.cfgPath, test.pipeName)
+		test.Send(t)
+		test.Validate(t)
+	}
 }
 
 // Struct for file-file plugin e2e test
@@ -64,16 +89,15 @@ type fileFile struct {
 	retTime           string
 }
 
-func (f *fileFile) AddConfigSettings(conf *cfg.Config, t *testing.T) {
+func (f *fileFile) AddConfigSettings(t *testing.T, conf *cfg.Config, pipeName string) {
 	f.filesDir = t.TempDir()
 	offsetsDir := t.TempDir()
-	fmt.Println(f.filesDir)
-	input := conf.Pipelines["test"].Raw.Get("input")
+	input := conf.Pipelines[pipeName].Raw.Get("input")
 	input.Set("watching_dir", f.filesDir)
 	input.Set("filename_pattern", f.filePattern)
 	input.Set("offsets_file", filepath.Join(offsetsDir, "offsets.yaml"))
 
-	output := conf.Pipelines["test"].Raw.Get("output")
+	output := conf.Pipelines[pipeName].Raw.Get("output")
 	output.Set("target_file", f.filesDir+f.outputNamePattern)
 	output.Set("retention_interval", f.retTime)
 }
@@ -97,6 +121,7 @@ func (f *fileFile) Send(t *testing.T) {
 		}()
 	}
 	wg.Wait()
+
 	// waiting for files to be processed
 	time.Sleep(time.Second * 10)
 }
@@ -106,4 +131,51 @@ func (f *fileFile) Validate(t *testing.T) {
 	matches := test.GetMatches(t, logFilePattern)
 	assert.True(t, len(matches) > 0, "There are no files")
 	require.Equal(t, f.count*f.lines, test.CountLines(t, logFilePattern))
+}
+
+// Struct for http-file plugin e2e test
+type httpFile struct {
+	filesDir          string
+	outputNamePattern string
+	count             int
+	lines             int
+	retTime           string
+}
+
+func (h *httpFile) AddConfigSettings(t *testing.T, conf *cfg.Config, pipeName string) {
+	h.filesDir = t.TempDir()
+	output := conf.Pipelines[pipeName].Raw.Get("output")
+	output.Set("target_file", h.filesDir+h.outputNamePattern)
+	output.Set("retention_interval", h.retTime)
+}
+
+func (h *httpFile) Send(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	wg.Add(h.count)
+	for i := 0; i < h.count; i++ {
+		go func() {
+			cl := http2.DefaultClient
+			for i := 0; i < h.lines; i++ {
+				rd := bytes.NewReader([]byte(`{"first_field":"second_field"}`))
+				req, err := http2.NewRequest(http2.MethodPost, "http://localhost:9200/", rd)
+				assert.Nil(t, err, "bad format http request")
+				_, err = cl.Do(req)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	// waiting for files to be processed
+	time.Sleep(time.Second * 10)
+}
+
+func (h *httpFile) Validate(t *testing.T) {
+	logFilePattern := fmt.Sprintf("%s/%s*%s", h.filesDir, "file-d", ".log")
+	matches := test.GetMatches(t, logFilePattern)
+	assert.True(t, len(matches) > 0, "There are no files")
+	require.Equal(t, h.count*h.lines, test.CountLines(t, logFilePattern))
 }
