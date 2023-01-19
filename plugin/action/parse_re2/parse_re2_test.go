ad215e1dbaa78be6134c1914364d6b369b7444e9
Dmitry Smolonogov <dsmolonogov@ozon.ru>
2023-01-19 14:20:18 +0300

    change field names

--- plugin/action/parse_re2/parse_re2_test.go
+++ plugin/action/parse_re2/parse_re2_test.go
@@ -49,8 +49,8 @@ func TestDecode(t *testing.T) {
 
 func TestDecodeAccessLogsJira(t *testing.T) {
 	config := &Config{
-		Field: "log",
-		Re2:   "(?P<ip>[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}) (?P<id>\\w+) (?P<username>(\\w+|-)) \\[(?P<date>[\\d]{2}/[[:alpha:]]{1,3}/[\\d]{4}:[\\d]{2}:[\\d]{2}:[\\d]{2} [+-][\\d]{4})\\] \"(?P<path>.+)\" (?P<num1>([\\d]+|-)) (?P<num2>([\\d]+|-)) (?P<num3>([\\d]+|-)) \"(?P<path_2>.+)\" \"(?P<browser>.+)\" \"(?P<username_2>.+)\"",
+		Field: "message",
+		Re2:   "(?P<origin_ip>[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}) (?P<request_id>\\w+) (?P<username>(\\w+|-)) \\[(?P<timestamp>[\\d]{2}/[[:alpha:]]{1,3}/[\\d]{4}:[\\d]{2}:[\\d]{2}:[\\d]{2} [+-][\\d]{4})\\] \"(?P<method_and_endpoint>.+)\" (?P<status_code>([\\d]+|-)) (?P<bytes_sent>([\\d]+|-)) (?P<processing_time>([\\d]+|-)) \"(?P<accessed_url>.+)\" \"(?P<client>.+)\" \"(?P<session_id>.+)\"",
 	}
 	p, input, output := test.NewPipelineMock(test.NewActionPluginStaticInfo(factory, config, pipeline.MatchModeAnd, nil, false))
 	wg := &sync.WaitGroup{}
@@ -70,16 +70,16 @@ func TestDecodeAccessLogsJira(t *testing.T) {
 	output.SetOutFn(func(e *pipeline.Event) {
 		outEvents = append(outEvents, e)
 		wg.Done()
-	})
+	})q
 
-	input.In(0, "test.log", 0, []byte(`{"log":"10.115.195.13 0x51320775x2 jira_robot [07/Nov/2022:00:00:00 +0300] \"GET /rest/api/2/issue/FRAUD-3847?fields=resolution HTTP/1.1\" 200 198 20 \"https://jit.o3.ru/secure/RapidBoard.jspa?rapidView=2701&selectedIssue=EXPC-3767&quickFilter=16465&quickFilter=15365\" \"Apache-HttpClient/4.5.13 (Java/11.0.9)\" \"nj56zg\""}`))
-	input.In(0, "test.log", 0, []byte(`{"log":"10.115.195.12 0x51320774x2 ezabelin [07/Nov/2022:00:00:00 +0300] \"GET /rest/api/2/issue/RP-4977?fields=resolution HTTP/1.1\" 201 158 15 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\" \"1tmznt9\""}`))
+	input.In(0, "test.log", 0, []byte(`{"message":"10.115.195.13 0x51320775x2 jira_robot [07/Nov/2022:00:00:00 +0300] \"GET /rest/api/2/issue/FRAUD-3847?fields=resolution HTTP/1.1\" 200 198 20 \"https://jit.o3.ru/secure/RapidBoard.jspa?rapidView=2701&selectedIssue=EXPC-3767&quickFilter=16465&quickFilter=15365\" \"Apache-HttpClient/4.5.13 (Java/11.0.9)\" \"nj56zg\""}`))
+	input.In(0, "test.log", 0, []byte(`{"message":"10.115.195.12 0x51320774x2 ezabelin [07/Nov/2022:00:00:00 +0300] \"GET /rest/api/2/issue/RP-4977?fields=resolution HTTP/1.1\" 201 158 15 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\" \"1tmznt9\""}`))
 
 	wg.Wait()
 	p.Stop()
 
 	assert.Equal(t, 2, inEvents, "wrong in events count")
 	assert.Equal(t, 2, len(outEvents), "wrong out events count")
-	assert.Equal(t, `{"ip":"10.115.195.13","id":"0x51320775x2","username":"jira_robot","date":"07/Nov/2022:00:00:00 +0300","path":"GET /rest/api/2/issue/FRAUD-3847?fields=resolution HTTP/1.1","num1":"200","num2":"198","num3":"20","path_2":"https://jit.o3.ru/secure/RapidBoard.jspa?rapidView=2701&selectedIssue=EXPC-3767&quickFilter=16465&quickFilter=15365","browser":"Apache-HttpClient/4.5.13 (Java/11.0.9)","username_2":"nj56zg"}`, outEvents[0].Root.EncodeToString(), "wrong out event")
-	assert.Equal(t, `{"ip":"10.115.195.12","id":"0x51320774x2","username":"ezabelin","date":"07/Nov/2022:00:00:00 +0300","path":"GET /rest/api/2/issue/RP-4977?fields=resolution HTTP/1.1","num1":"201","num2":"158","num3":"15","path_2":"-","browser":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36","username_2":"1tmznt9"}`, outEvents[1].Root.EncodeToString(), "wrong out event")
+	assert.Equal(t, `{"origin_ip":"10.115.195.13","request_id":"0x51320775x2","username":"jira_robot","timestamp":"07/Nov/2022:00:00:00 +0300","method_and_endpoint":"GET /rest/api/2/issue/FRAUD-3847?fields=resolution HTTP/1.1","status_code":"200","bytes_sent":"198","processing_time":"20","accessed_url":"https://jit.o3.ru/secure/RapidBoard.jspa?rapidView=2701&selectedIssue=EXPC-3767&quickFilter=16465&quickFilter=15365","client":"Apache-HttpClient/4.5.13 (Java/11.0.9)","session_id":"nj56zg"}`, outEvents[0].Root.EncodeToString(), "wrong out event")
+	assert.Equal(t, `{"origin_ip":"10.115.195.12","request_id":"0x51320774x2","username":"ezabelin","timestamp":"07/Nov/2022:00:00:00 +0300","method_and_endpoint":"GET /rest/api/2/issue/RP-4977?fields=resolution HTTP/1.1","status_code":"201","bytes_sent":"158","processing_time":"15","accessed_url":"-","client":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36","session_id":"1tmznt9"}`, outEvents[1].Root.EncodeToString(), "wrong out event")
 }
