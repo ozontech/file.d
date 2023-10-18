74dcaec6dee086bee5ad9db9ad245b995d463363
Oleg Don <31502412+HeadHunter483@users.noreply.github.com>
2023-10-18 23:18:34 +0500

    Add regex filter to substitutions

--- cfg/substitution.go
+++ cfg/substitution.go
@@ -1,8 +1,13 @@
 package cfg
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"regexp"
 	"strings"
+
+	"go.uber.org/zap"
 )
 
 type SubstitutionOpKind int
@@ -10,14 +15,137 @@ type SubstitutionOpKind int
 const (
 	SubstitutionOpKindRaw SubstitutionOpKind = iota
 	SubstitutionOpKindField
+
+	regexFilterPrefix = "re("
+
+	bufInitSize = 1024
 )
 
-type SubstitutionOp struct {
-	Kind SubstitutionOpKind
-	Data []string
+type quotesOpenStatus int
+
+const (
+	quotesOpenedNo quotesOpenStatus = iota
+	quotesOpenedSingle
+	quotesOpenedDouble
+)
+
+var errInvalidFilter = errors.New("invalid filter")
+
+type FieldFilter interface {
+	Apply([]byte) []byte
+
+	setBuffer([]byte)
+	compareArgs([]any) error
 }
 
-func ParseSubstitution(substitution string) ([]SubstitutionOp, error) {
+type RegexFilter struct {
+	re        *regexp.Regexp
+	groups    []int
+	separator []byte
+
+	buf []byte
+}
+
+func (r *RegexFilter) Apply(data []byte) []byte {
+	if len(r.groups) == 0 {
+		return data
+	}
+	indexes := r.re.FindAllSubmatchIndex(data, -1)
+	if len(indexes) == 0 {
+		return data
+	}
+	r.buf = r.buf[:0]
+	for _, index := range indexes {
+		for _, grp := range r.groups {
+			start := index[grp*2]
+			end := index[grp*2+1]
+			if len(r.separator) > 0 && len(r.buf) != 0 {
+				r.buf = append(r.buf, r.separator...)
+			}
+			r.buf = append(r.buf, data[start:end]...)
+		}
+	}
+	return r.buf
+}
+
+func (r *RegexFilter) setBuffer(buf []byte) {
+	r.buf = buf
+}
+
+// compareArgs is used for testing. Checks filter args values.
+func (r *RegexFilter) compareArgs(args []any) error {
+	if len(args) != 3 {
+		return fmt.Errorf("wrong regex filter amount of args, want=%d got=%d", 3, len(args))
+	}
+	wantRe := args[0].(string)
+	gotRe := r.re.String()
+	if wantRe != gotRe {
+		return fmt.Errorf("wrong regex filter regex expr, want=%q got=%q", wantRe, gotRe)
+	}
+	wantGroups := args[1].([]int)
+	gotGroups := r.groups
+	if len(wantGroups) != len(gotGroups) {
+		return fmt.Errorf("wrong regex filter groups, want=%v got=%v", wantGroups, gotGroups)
+	}
+	for i := 0; i < len(wantGroups); i++ {
+		if wantGroups[i] != gotGroups[i] {
+			return fmt.Errorf("wrong regex filter groups, want=%v got=%v", wantGroups, gotGroups)
+		}
+	}
+	wantSeparator := args[2].(string)
+	gotSeparator := string(r.separator)
+	if wantSeparator != gotSeparator {
+		return fmt.Errorf("wrong regex filter separator, want=%q got=%q", wantSeparator, gotSeparator)
+	}
+	return nil
+}
+
+// indexRuneInExpr same as strings.IndexByte but depending on flags
+// considers sought rune is outside quotes and whether it is escaped explicitly
+// using separate escaping slash (e.g. "\\d")
+func indexRuneInExpr(expr string, c rune, considerQuotes, considerEscaped bool) int {
+	if !considerQuotes && considerEscaped {
+		return strings.IndexByte(expr, byte(c))
+	}
+	quotesOpened := quotesOpenedNo
+searchLoop:
+	for i, b := range expr {
+		if considerQuotes && (i == 0 || expr[i-1] != '\\') {
+			switch {
+			case quotesOpened == quotesOpenedNo:
+				if b == '\'' {
+					quotesOpened = quotesOpenedSingle
+					continue searchLoop
+				} else if b == '"' {
+					quotesOpened = quotesOpenedDouble
+					continue searchLoop
+				}
+			case quotesOpened == quotesOpenedSingle && b == '\'':
+				quotesOpened = quotesOpenedNo
+				continue
+			case quotesOpened == quotesOpenedDouble && b == '"':
+				quotesOpened = quotesOpenedNo
+				continue
+			}
+			// when quotes are considered skip all bytes inside quotes
+			if quotesOpened != quotesOpenedNo {
+				continue
+			}
+		}
+		if b == c && (considerEscaped || i == 0 || expr[i-1] != '\\') {
+			return i
+		}
+	}
+	return -1
+}
+
+type SubstitutionOp struct {
+	Kind    SubstitutionOpKind
+	Data    []string
+	Filters []FieldFilter
+}
+
+func ParseSubstitution(substitution string, logger *zap.Logger) ([]SubstitutionOp, error) {
 	result := make([]SubstitutionOp, 0)
 	tail := ""
 	for {
@@ -42,16 +170,39 @@ func ParseSubstitution(substitution string) ([]SubstitutionOp, error) {
 			})
 			tail = ""
 
-			end := strings.IndexByte(substitution, '}')
+			end := indexRuneInExpr(substitution, '}', true, false)
 			if end == -1 {
 				return nil, fmt.Errorf("can't find substitution end '}': %s", substitution)
 			}
 
-			selector := substitution[pos+2 : end]
+			var filterOps []FieldFilter
+			selectorEnd := end
+			pipe := indexRuneInExpr(substitution, '|', true, false)
+			if pipe != -1 {
+				filterBuf := make([]byte, 0, bufInitSize)
+				selectorEnd = pipe
+				offset := 0
+				for pipe != -1 {
+					pipe += offset
+					filterOp, filterEndPos, err := parseFilter(substitution[pipe+1:end], logger)
+					if err != nil {
+						return nil, err
+					}
+					// single buffer for all filters because there is only one event for a substitution op simultaneously
+					// and filters in substitution op are applied sequentially one by one
+					filterOp.setBuffer(filterBuf)
+					filterOps = append(filterOps, filterOp)
+					offset = pipe + 1 + filterEndPos
+					pipe = indexRuneInExpr(substitution[offset:end], '|', true, false)
+				}
+			}
+
+			selector := substitution[pos+2 : selectorEnd]
 			path := ParseFieldSelector(selector)
 			result = append(result, SubstitutionOp{
-				Kind: SubstitutionOpKindField,
-				Data: path,
+				Kind:    SubstitutionOpKindField,
+				Data:    path,
+				Filters: filterOps,
 			})
 
 			substitution = substitution[end+1:]
@@ -69,4 +220,119 @@ func ParseSubstitution(substitution string) ([]SubstitutionOp, error) {
 	}
 
 	return result, nil
+}
+
+// parseFilter parses filter data from string with filter args if present in format "<filter-name>(<arg1>, <arg2>, ...)".
+func parseFilter(data string, logger *zap.Logger) (FieldFilter, int, error) {
+	filterEndPos := -1
+	origDataLen := len(data)
+	data = strings.TrimLeft(data, " ")
+	offset := origDataLen - len(data)
+	if strings.HasPrefix(data, regexFilterPrefix) {
+		args, argsEndPos, err := parseFilterArgs(data[len(regexFilterPrefix):])
+		if err != nil {
+			return nil, filterEndPos, fmt.Errorf("failed to parse filter args: %w", err)
+		}
+		filterEndPos = argsEndPos + len(regexFilterPrefix) + offset
+		if len(args) != 3 {
+			return nil, filterEndPos, fmt.Errorf("invalid args for regexp filter, exptected 3, got %d", len(args))
+		}
+		var reStr string
+		var groups []int
+		var separator string
+		if err := json.Unmarshal([]byte(args[0]), &reStr); err != nil {
+			return nil, filterEndPos, fmt.Errorf("failed to parse regexp filter regexp string: %w", err)
+		}
+		re := regexp.MustCompile(reStr)
+		if err := json.Unmarshal([]byte(args[1]), &groups); err != nil {
+			return nil, filterEndPos, fmt.Errorf("failed to parse regexp filter groups: %w", err)
+		}
+		VerifyGroupNumbers(groups, re.NumSubexp(), logger)
+		if err := json.Unmarshal([]byte(args[2]), &separator); err != nil {
+			return nil, filterEndPos, fmt.Errorf("failed to parse regexp filter separator: %w", err)
+		}
+		filter := &RegexFilter{
+			re:        re,
+			groups:    groups,
+			separator: []byte(separator),
+		}
+		return filter, filterEndPos, nil
+	}
+	return nil, filterEndPos, errInvalidFilter
+}
+
+// parseFilterArgs parses args from string in format of "<arg1>, <arg2>, ...)" --
+// filter args without its name and starting bracket.
+func parseFilterArgs(data string) ([]string, int, error) {
+	var args []string
+	// no args
+	if data[0] == ')' {
+		return args, 0, nil
+	}
+	argsEndPos := -1
+	quotesOpened := quotesOpenedNo
+	brackets := make([]byte, 0, 100)
+	curArg := make([]byte, 0, 100)
+	// parse args separated by comma ','
+	// re("((),()),()", [1, 2, 3], "'(,)'")
+gatherLoop:
+	for i, b := range []byte(data) {
+		// escaped characters handling
+		if i != 0 && data[i-1] == '\\' {
+			curArg = append(curArg, b)
+			continue
+		}
+		// quotes have a priority over brackets
+		switch {
+		case quotesOpened == quotesOpenedNo:
+			switch {
+			case b == '\'':
+				quotesOpened = quotesOpenedSingle
+			case b == '"':
+				quotesOpened = quotesOpenedDouble
+			case b == '(' || b == '[' || b == '{':
+				brackets = append(brackets, b)
+			case b == ')' && len(brackets) == 0:
+				// early stop on the end of args list
+				argsEndPos = i
+				break gatherLoop
+			case b == ')' || b == ']':
+				lastBracket := brackets[len(brackets)-1]
+				switch {
+				case b == ')' && lastBracket != '(':
+					return nil, argsEndPos, fmt.Errorf("invalid brackets: %s", data)
+				case b == ']' && lastBracket != '[':
+					return nil, argsEndPos, fmt.Errorf("invalid brackets: %s", data)
+				}
+				brackets = brackets[:len(brackets)-1]
+			case b == ',' && len(brackets) == 0:
+				// condition for separating args
+				args = append(args, string(curArg))
+				curArg = curArg[:0]
+				continue gatherLoop
+			}
+		case b == '\'' && quotesOpened == quotesOpenedSingle:
+			quotesOpened = quotesOpenedNo
+		case b == '"' && quotesOpened == quotesOpenedDouble:
+			quotesOpened = quotesOpenedNo
+		}
+		curArg = append(curArg, b)
+	}
+	if len(brackets) > 0 {
+		return nil, argsEndPos, fmt.Errorf("invalid brackets: %s", data)
+	}
+	if quotesOpened != quotesOpenedNo {
+		return nil, argsEndPos, fmt.Errorf("not all quotes are closed: %s", data)
+	}
+	if argsEndPos == -1 {
+		return nil, argsEndPos, fmt.Errorf("no closing bracket for args list: %s", data)
+	}
+	// last arg
+	if len(curArg) > 0 {
+		args = append(args, string(curArg))
+	}
+	for i := range args {
+		args[i] = strings.TrimSpace(args[i])
+	}
+	return args, argsEndPos, nil
 }
--- cfg/substitution_test.go
+++ cfg/substitution_test.go
@@ -3,11 +3,15 @@ package cfg
 import (
 	"testing"
 
+	"github.com/ozontech/file.d/logger"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+var lg = logger.Instance.Desugar()
+
 func TestParseVale(t *testing.T) {
-	result, err := ParseSubstitution("just value")
+	result, err := ParseSubstitution("just value", lg)
 
 	assert.NoError(t, err, "error occurs")
 	assert.Equal(t, 1, len(result), "wrong result")
@@ -15,7 +19,7 @@ func TestParseVale(t *testing.T) {
 }
 
 func TestParseField(t *testing.T) {
-	result, err := ParseSubstitution("days till world end ${prediction.days}. so what?")
+	result, err := ParseSubstitution("days till world end ${prediction.days}. so what?", lg)
 
 	assert.NoError(t, err, "error occurs")
 	assert.Equal(t, 3, len(result), "wrong result")
@@ -26,7 +30,7 @@ func TestParseField(t *testing.T) {
 }
 
 func TestParseEnding(t *testing.T) {
-	result, err := ParseSubstitution("days till world end ${prediction.days}")
+	result, err := ParseSubstitution("days till world end ${prediction.days}", lg)
 
 	assert.NoError(t, err, "error occurs")
 	assert.Equal(t, 2, len(result), "wrong result")
@@ -36,7 +40,7 @@ func TestParseEnding(t *testing.T) {
 }
 
 func TestParseEscape(t *testing.T) {
-	result, err := ParseSubstitution("days till world end $$100")
+	result, err := ParseSubstitution("days till world end $$100", lg)
 
 	assert.NoError(t, err, "error occurs")
 	assert.Equal(t, 1, len(result), "wrong result")
@@ -44,7 +48,175 @@ func TestParseEscape(t *testing.T) {
 }
 
 func TestParseErr(t *testing.T) {
-	_, err := ParseSubstitution("days till world end ${prediction.days. so what?")
+	_, err := ParseSubstitution("days till world end ${prediction.days. so what?", lg)
 
 	assert.NotNil(t, err, "no error")
+}
+
+func TestParseFieldWithFilter(t *testing.T) {
+	tests := []struct {
+		name         string
+		substitution string
+		data         [][]string
+		filters      [][][]any
+		wantErr      bool
+	}{
+		{
+			name:         "with_one_filter",
+			substitution: `days till world end ${prediction.days|re("(\\d),(test.+)",[1,2]," , ")}. so what?`,
+			data: [][]string{
+				{"days till world end "},
+				{"prediction", "days"},
+				{". so what?"},
+			},
+			filters: [][][]any{
+				nil,
+				{
+					{
+						"(\\d),(test.+)",
+						[]int{1, 2},
+						" , ",
+					},
+				},
+				nil,
+			},
+			wantErr: false,
+		},
+		{
+			name:         "with_two_filters",
+			substitution: `days till world end ${prediction.days|re("(\\d),(test.+)",[1,2]," , ") | re("(test2\\.subtest)", [1], "-||-")}. so what?`,
+			data: [][]string{
+				{"days till world end "},
+				{"prediction", "days"},
+				{". so what?"},
+			},
+			filters: [][][]any{
+				nil,
+				{
+					{
+						"(\\d),(test.+)",
+						[]int{1, 2},
+						" , ",
+					},
+					{
+						"(test2\\.subtest)",
+						[]int{1},
+						"-||-",
+					},
+				},
+				nil,
+			},
+			wantErr: false,
+		},
+		{
+			name:         "with_two_substitutions_one_filter",
+			substitution: `days till world end ${prediction.days|re("(\\d),(test.+)",[1,2]," , ")}. Hello, ${name|re("(\\w+)",[1],",")}`,
+			data: [][]string{
+				{"days till world end "},
+				{"prediction", "days"},
+				{". Hello, "},
+				{"name"},
+			},
+			filters: [][][]any{
+				nil,
+				{
+					{
+						"(\\d),(test.+)",
+						[]int{1, 2},
+						" , ",
+					},
+				},
+				nil,
+				{
+					{
+						"(\\w+)",
+						[]int{1},
+						",",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:         "err_invalid_filter",
+			substitution: `test ${field|abcd()} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_empty",
+			substitution: `test ${field|re()} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_invalid_first_arg",
+			substitution: `test ${field|re('(invalid)',[1,],"|")} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_invalid_second_arg",
+			substitution: `test ${field|re("invalid",[invalid],"|")} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_invalid_third_arg",
+			substitution: `test ${field|re("(invalid)",[1],'invalid')} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_no_last_bracket",
+			substitution: `test ${field|re('invalid'} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_bracket_not_closed",
+			substitution: `test ${field|re('invalid', [(1,2, "|")} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_invalid_bracket_sequence",
+			substitution: `test ${field|re('invalid', [1,2, "|")} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_invalid_bracket_sequence2",
+			substitution: `test ${field|re('invalid', [(1,2], "|")} test2`,
+			wantErr:      true,
+		},
+		{
+			name:         "err_invalid_args_no_closing_quotes",
+			substitution: `test ${field|re("invalid", [1,2], "|)} test2`,
+			wantErr:      true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			result, err := ParseSubstitution(tt.substitution, lg)
+			require.Equal(t, tt.wantErr, err != nil, "no error was expected")
+			assert.Equal(t, len(tt.data), len(result), "invalid substitution ops len")
+			for j := range tt.data {
+				assert.Equal(t, len(tt.data[j]), len(result[j].Data), "invalid data len")
+				for k := range tt.data[j] {
+					assert.Equal(t, tt.data[j][k], result[j].Data[k], "wrong data for")
+				}
+				if tt.filters[j] == nil {
+					assert.Nil(t, result[j].Filters)
+				} else {
+					assert.Equal(t, len(tt.filters[j]), len(result[j].Filters), "invalid filters len")
+					for k := range tt.filters[j] {
+						assert.NoError(t, result[j].Filters[k].compareArgs(tt.filters[j][k]), "wrong args for filter")
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestRegexFilterApply(t *testing.T) {
+	result, err := ParseSubstitution(`test ${field|re("(re\\d)",[1],"|")}`, lg)
+	require.NoError(t, err)
+	res := result[1].Filters[0].Apply([]byte(`this is some text re1 end`))
+	assert.Equal(t, "re1", string(res))
 }
