ca4a12f13babcf252be2ed64f3b9aa2b4dd8e899
Oleg Don <31502412+HeadHunter483@users.noreply.github.com>
2023-11-28 23:05:39 +0500

    Update modify plugin

--- plugin/action/modify/modify.go
+++ plugin/action/modify/modify.go
@@ -7,6 +7,8 @@ import (
 	"go.uber.org/zap"
 )
 
+const filterBufInitSize = 1024
+
 /*{ introduction
 It modifies the content for a field. It works only with strings.
 You can provide an unlimited number of config parameters. Each parameter handled as `cfg.FieldSelector`:`cfg.Substitution`.
@@ -37,10 +39,11 @@ The resulting event could look like:
 }*/
 
 type Plugin struct {
-	config *Config
-	logger *zap.Logger
-	ops    map[string][]cfg.SubstitutionOp
-	buf    []byte
+	config   *Config
+	logger   *zap.Logger
+	ops      map[string][]cfg.SubstitutionOp
+	buf      []byte
+	fieldBuf []byte
 }
 
 type Config map[string]string
@@ -61,8 +64,11 @@ func (p *Plugin) Start(config pipeline.AnyConfig, params *pipeline.ActionPluginP
 	p.ops = make(map[string][]cfg.SubstitutionOp)
 	p.logger = params.Logger.Desugar()
 
+	filtersBuf := make([]byte, 0, filterBufInitSize)
 	for key, value := range *p.config {
-		ops, err := cfg.ParseSubstitution(value, p.logger)
+		// if there are field filters in substitutions, they will have single buffer for all
+		// substitution ops in this plugin
+		ops, err := cfg.ParseSubstitution(value, filtersBuf, p.logger)
 		if err != nil {
 			p.logger.Fatal("can't parse substitution", zap.Error(err))
 		}
@@ -86,11 +92,13 @@ func (p *Plugin) Do(event *pipeline.Event) pipeline.ActionResult {
 			case cfg.SubstitutionOpKindRaw:
 				p.buf = append(p.buf, op.Data[0]...)
 			case cfg.SubstitutionOpKindField:
+				p.fieldBuf = p.fieldBuf[:0]
 				fieldData := event.Root.Dig(op.Data...).AsBytes()
+				p.fieldBuf = append(p.fieldBuf, fieldData...)
 				for i := 0; i < len(op.Filters); i++ {
-					fieldData = op.Filters[i].Apply(fieldData)
+					p.fieldBuf = op.Filters[i].Apply(p.fieldBuf, p.fieldBuf)
 				}
-				p.buf = append(p.buf, fieldData...)
+				p.buf = append(p.buf, p.fieldBuf...)
 			default:
 				p.logger.Panic("unknown substitution kind", zap.Int("substitution_kind", int(op.Kind)))
 			}
