729690489557aa66d9bf45de781773fb74157da0
Vadim Alekseev <vadimaleksv@gmail.com>
2024-01-09 14:03:15 +0300

    Benchmark prometheus.MetricVec

--- metric/held_metric_test.go
+++ metric/held_metric_test.go
@@ -8,42 +8,62 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func BenchmarkMetricWithLabels(b *testing.B) {
-	benchCases := []struct {
-		Labels      []string
-		LabelValues [][]string
-	}{
-		{
-			Labels: []string{"first"},
-			LabelValues: [][]string{
-				{"test1"},
-				{"test2"},
-				{"test3"},
-			},
+var holderBenchCases = []struct {
+	Labels      []string
+	LabelValues [][]string
+}{
+	{
+		Labels: []string{"l1"},
+		LabelValues: [][]string{
+			{"test1"},
+			{"test2"},
+			{"test3"},
 		},
-		{
-			Labels: []string{"first", "second"},
-			LabelValues: [][]string{
-				{"first1", "second1"},
-				{"first2", "second2"},
-				{"first3", "second3"},
-			},
+	},
+	{
+		Labels: []string{"l1", "l2"},
+		LabelValues: [][]string{
+			{"first1", "second1"},
+			{"first2", "second2"},
+			{"first3", "second3"},
 		},
-		{
-			Labels: []string{"first", "second", "third"},
-			LabelValues: [][]string{
-				{"first1", "second1", "third1"},
-				{"first2", "second2", "third2"},
-				{"first3", "second3", "third3"},
-			},
+	},
+	{
+		Labels: []string{"l1", "l2", "l3"},
+		LabelValues: [][]string{
+			{"first1", "second1", "third1"},
+			{"first2", "second2", "third2"},
+			{"first3", "second3", "third3"},
 		},
-	}
+	},
+}
 
-	for _, benchCase := range benchCases {
+func BenchmarkMetricHolder(b *testing.B) {
+	for _, benchCase := range holderBenchCases {
 		ctl := NewCtl("test", prometheus.NewRegistry())
 		holder := NewHolder(time.Minute)
-		counter := holder.AddCounterVec(ctl.RegisterCounterVec("test_name", "", benchCase.Labels...))
 
+		promCounter := ctl.RegisterCounterVec("test_name", "", benchCase.Labels...)
+		counter := holder.AddCounterVec(promCounter)
+
+		name := strings.Join(benchCase.Labels, "_")
+
+		b.Run(name, func(b *testing.B) {
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					for _, labels := range benchCase.LabelValues {
+						counter.WithLabelValues(labels...).Inc()
+					}
+				}
+			})
+		})
+	}
+}
+
+func BenchmarkPromVec(b *testing.B) {
+	for _, benchCase := range holderBenchCases {
+		ctl := NewCtl("test", prometheus.NewRegistry())
+		counter := ctl.RegisterCounterVec("test_name", "", benchCase.Labels...)
 		name := strings.Join(benchCase.Labels, "_")
 
 		b.Run(name, func(b *testing.B) {
