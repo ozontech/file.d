ecf41468e276ffa4c005cde71d6393493c464bf8
Dmitry Romanov <dmitry.romanov85@protonmail.com>
2024-07-23 19:30:36 +0700

    add TestAssembleOffset

--- plugin/input/kafka/kafka_test.go
+++ plugin/input/kafka/kafka_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 func TestAssembleSourceID(t *testing.T) {
@@ -16,4 +17,17 @@ func TestAssembleSourceID(t *testing.T) {
 
 	assert.Equal(t, index, newIndex, "values aren't equal")
 	assert.Equal(t, partition, newPartition, "values aren't equal")
+}
+
+func TestAssembleOffset(t *testing.T) {
+	message := &kgo.Record{
+		LeaderEpoch: 27,
+		Offset:      237582035700,
+	}
+	x := assembleOffset(message)
+
+	epochOffset := disassembleOffset(x)
+
+	assert.Equal(t, message.LeaderEpoch, epochOffset.Epoch, "values aren't equal")
+	assert.Equal(t, message.Offset+1, epochOffset.Offset, "values aren't equal")
 }
