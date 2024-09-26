c32af4e935c58464b9ef5e4df3e278080d1af91a
Dmitry Romanov <dmitry.romanov85@protonmail.com>
2024-09-26 19:05:05 +0700

    Revert "release pool memory on spawn"
    This reverts commit 977e63ead9063a40f1fcc686742869c3b8be18d0.

--- pipeline/processor.go
+++ pipeline/processor.go
@@ -414,7 +414,6 @@ func (p *processor) Spawn(parent *Event, nodes []*insaneJSON.Node) {
 			child.stage = eventStageOutput
 			p.output.Out(child)
 		}
-		child.Root.ReleasePoolMem()
 	}
 
 	if p.busyActionsTotal == 0 {
