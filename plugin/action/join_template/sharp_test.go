a8c78317b14310e72f15912257da56a76ebd06fa
george pogosyan <gepogosyan@ozon.ru>
2025-01-28 14:54:40 +0300

    Remove duplicating test

--- plugin/action/join_template/sharp_test.go
+++ plugin/action/join_template/sharp_test.go
@@ -6,11 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type testCase struct {
-	s   string
-	res bool
-}
-
 func TestSharpStart(t *testing.T) {
 	tests := []testCase{
 		// positive
@@ -26,26 +21,5 @@ func TestSharpStart(t *testing.T) {
 
 	for i, test := range tests {
 		require.Equal(t, test.res, sharpStartCheck(test.s), i)
-	}
-}
-
-func TestContainsOnlySpaces(t *testing.T) {
-	tests := []testCase{
-		{s: "    ", res: true},
-		{s: "\t\t", res: true},
-		{s: "\n\n", res: true},
-		{s: " \n\t ", res: true},
-		{s: "", res: true},
-
-		// negative
-		{s: "qwe"},
-		{s: "a   "},
-		{s: "   a"},
-		{s: "\n\t a"},
-		{s: "a\n\t "},
-	}
-
-	for i, test := range tests {
-		require.Equal(t, test.res, containsOnlySpaces(test.s), i)
 	}
 }
