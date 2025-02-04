da24d778761d4e6dd570ffd8ffc2e4148c4a3946
george pogosyan <gepogosyan@ozon.ru>
2025-02-05 00:32:52 +0300

    Refactor

--- plugin/action/keep_fields/keep_fields_test.go
+++ plugin/action/keep_fields/keep_fields_test.go
@@ -1,12 +1,15 @@
 package keep_fields
 
 import (
+	"math/rand"
+	"slices"
 	"sync"
 	"testing"
 
 	"github.com/ozontech/file.d/pipeline"
 	"github.com/ozontech/file.d/test"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestKeepFields(t *testing.T) {
@@ -32,4 +35,42 @@ func TestKeepFields(t *testing.T) {
 	assert.Equal(t, `{"field_1":"value_1"}`, outEvents[0], "wrong event")
 	assert.Equal(t, `{"field_2":"value_2"}`, outEvents[1], "wrong event")
 	assert.Equal(t, `{}`, outEvents[2], "wrong event")
+}
+
+func BenchmarkGenerics(b *testing.B) {
+	a := make([]int, 10_000)
+	for i := range a {
+		a[i] = rand.Int()
+	}
+
+	for i := 0; i < len(a)-1; i++ {
+		if a[i] == a[len(a)-1] {
+			a[i]++
+		}
+	}
+
+	b.ResetTimer()
+
+	b.Run("generic", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			res := slices.Index(a, a[len(a)-1])
+			require.True(b, res != -1)
+		}
+	})
+	b.Run("explicit", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			res := find(a, a[len(a)-1])
+			require.True(b, res != -1)
+		}
+	})
+}
+
+func find(a []int, x int) int {
+	for i, elem := range a {
+		if elem == x {
+			return i
+		}
+	}
+
+	return -1
 }
