d41e7b35e04fff81f12e22c83b27ca1cc75f167d
george pogosyan <gepogosyan@ozon.ru>
2025-02-19 21:28:22 +0300

    Fix test

--- plugin/action/keep_fields/keep_fields_test.go
+++ plugin/action/keep_fields/keep_fields_test.go
@@ -1,9 +1,6 @@
 package keep_fields
 
 import (
-	"math/rand"
-	"slices"
-	"strings"
 	"sync"
 	"testing"
 
@@ -35,53 +32,4 @@ func TestKeepFields(t *testing.T) {
 	require.Equal(t, `{"field_1":"value_1"}`, outEvents[0], "wrong event")
 	require.Equal(t, `{"field_2":"value_2"}`, outEvents[1], "wrong event")
 	require.Equal(t, `{}`, outEvents[2], "wrong event")
-}
-
-func BenchmarkGenerics(b *testing.B) {
-	a := getRandLines()
-	s := a[len(a)-1]
-
-	b.ResetTimer()
-
-	b.Run("generic", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			_ = slices.Index(a, s)
-		}
-	})
-	b.Run("explicit", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			_ = find(a, s)
-		}
-	})
-}
-
-func getRandLines() []string {
-	const count = 1000
-
-	lines := make([]string, 0, count)
-	for i := 0; i < count; i++ {
-		lines = append(lines, getRandLine())
-	}
-
-	return lines
-}
-
-func getRandLine() string {
-	const (
-		maxLength = 1000
-
-		from = ' '
-		to   = '~'
-	)
-
-	sz := rand.Intn(maxLength) + 1
-
-	var b strings.Builder
-	b.Grow(sz)
-
-	for i := 0; i < sz; i++ {
-		b.WriteByte(from + byte(rand.Intn(to-from)))
-	}
-
-	return b.String()
 }
