132ac9a8f3d162c917a28389e5c1f9179dc79e3f
george pogosyan <gepogosyan@ozon.ru>
2025-03-21 17:24:11 +0300

    Use cfg.ParseNestedFields func

--- plugin/action/keep_fields/keep_fields_test.go
+++ plugin/action/keep_fields/keep_fields_test.go
@@ -66,37 +66,6 @@ func TestKeepNestedFields(t *testing.T) {
 	require.Equal(t, `{"f":"k"}`, outEvents[2], "wrong event")
 }
 
-func TestKeepFieldsEmptyConfig(t *testing.T) {
-	config := test.NewConfig(&Config{Fields: []string{}}, nil)
-	p, input, output := test.NewPipelineMock(test.NewActionPluginStaticInfo(factory, config, pipeline.MatchModeAnd, nil, false))
-	wg := &sync.WaitGroup{}
-	wg.Add(3)
-
-	outEvents := make([]string, 0, 3)
-	output.SetOutFn(func(e *pipeline.Event) {
-		outEvents = append(outEvents, e.Root.EncodeToString())
-		wg.Done()
-	})
-
-	input.In(0, "test.log", test.NewOffset(0),
-		[]byte(`{"a":{"b":{"c":1,"d":1}},"d":1}`),
-	)
-	input.In(0, "test.log", test.NewOffset(0),
-		[]byte(`{"a":{"b":[1,2],"d":1}}`),
-	)
-	input.In(0, "test.log", test.NewOffset(0),
-		[]byte(`{"a":{"g":"h","i":"j","f":"nested"},"f":"k"}`),
-	)
-
-	wg.Wait()
-	p.Stop()
-
-	require.Equal(t, 3, len(outEvents), "wrong out events count")
-	require.Equal(t, `{}`, outEvents[0], "wrong event")
-	require.Equal(t, `{}`, outEvents[1], "wrong event")
-	require.Equal(t, `{}`, outEvents[2], "wrong event")
-}
-
 const dataNested = `
 {
 	"level11": "qwerty",
@@ -265,74 +234,5 @@ func TestKeepNestedFieldsNoSaved(t *testing.T) {
 		root, _ := insaneJSON.DecodeBytes([]byte(data.out))
 		want := root.EncodeToString()
 		require.Equal(t, want, outEvents[i], "wrong event")
-	}
-}
-
-func TestParseNestedFields(t *testing.T) {
-	type TestCase struct {
-		in  []string
-		out [][]string
-	}
-
-	for _, tt := range []TestCase{
-		{
-			// simple
-			in:  []string{"a", "b", "c"},
-			out: [][]string{{"a"}, {"b"}, {"c"}},
-		},
-		{
-			// empty
-			in:  []string{"", "a", "", "", "b", ""},
-			out: [][]string{{"a"}, {"b"}},
-		},
-		{
-			// simple duplicates
-			in:  []string{"a", "b", "b", "a", "c", "a", "c", "b"},
-			out: [][]string{{"a"}, {"b"}, {"c"}},
-		},
-		{
-			// nested duplicates
-			in:  []string{"a.b", "some.qwe", "some.qwe", "a.b"},
-			out: [][]string{{"a", "b"}, {"some", "qwe"}},
-		},
-		{
-			// nested duplicates and nested field
-			in:  []string{"a.b.c", "a.b", "a.b"},
-			out: [][]string{{"a", "b"}},
-		},
-		{
-			// field name prefix
-			in:  []string{"prefix", "prefix_some"},
-			out: [][]string{{"prefix"}, {"prefix_some"}},
-		},
-		{
-			// nested field name prefix
-			in:  []string{"qwe12.some.f1", "qwe"},
-			out: [][]string{{"qwe"}, {"qwe12", "some", "f1"}},
-		},
-		{
-			// nested field name prefix
-			in:  []string{"qwe.some12.f1", "qwe.some"},
-			out: [][]string{{"qwe", "some"}, {"qwe", "some12", "f1"}},
-		},
-		{
-			// many nested fields
-			in:  []string{"a.b", "a.b.c", "a.d", "a"},
-			out: [][]string{{"a"}},
-		},
-		{
-			in:  []string{"a.b.f1", "a.b.f2"},
-			out: [][]string{{"a", "b", "f1"}, {"a", "b", "f2"}},
-		},
-		{
-			in:  []string{"a.b.f1", "a.b.f2", "a.b", "a.c"},
-			out: [][]string{{"a", "b"}, {"a", "c"}},
-		},
-		{
-			in:  []string{"a.b.f1", "a.b.f2", "a.b", "a.c", "a"},
-			out: [][]string{{"a"}},
-		},
-	} {
-		require.Equal(t, tt.out, parseNestedFields(tt.in))
 	}
 }
