4171d53fe4cb70b3e770d3d1c35d719f3352cd47
Yaroslav Kirillov <kirillov.y6@gmail.com>
2025-04-30 14:07:17 +0500

    Add normalize patterns in hash plugin (#831)
    * Add normalize patterns in hash plugin
    
    * Fix doc
    
    * Add test
    
    * Rework builtin_patterns config format
    
    * Add escaping in quotes
    
    * Add grave_quoted pattern
    
    * Fix doc
    
    * Fix review comments

--- plugin/action/hash/normalize/token_normalizer.go
+++ plugin/action/hash/normalize/token_normalizer.go
@@ -3,6 +3,7 @@ package normalize
 import (
 	"errors"
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/timtadh/lexmachine"
@@ -14,93 +15,220 @@ const (
 	patternPriorityLast  = "last"
 )
 
+const (
+	pAll = -1
+	pNo  = 0
+
+	pCurlyBracketed  = 1 << (iota - 2) // {...}
+	pSquareBracketed                   // [...]
+	pParenthesized                     // (...)
+	pDoubleQuoted                      // "..."
+	pSingleQuoted                      // '...'
+	pGraveQuoted                       // `...`
+	pEmail
+	pUrl
+	pHost
+	pUuid
+	pSha1
+	pMd5
+	pDatetime
+	pIp
+	pDuration
+	pHex
+	pFloat
+	pInt
+	pBool
+)
+
+var patternById = map[string]int{
+	"all": pAll,
+	"no":  pNo,
+
+	"curly_bracketed":  pCurlyBracketed,
+	"square_bracketed": pSquareBracketed,
+	"parenthesized":    pParenthesized,
+	"double_quoted":    pDoubleQuoted,
+	"single_quoted":    pSingleQuoted,
+	"grave_quoted":     pGraveQuoted,
+	"email":            pEmail,
+	"url":              pUrl,
+	"host":             pHost,
+	"uuid":             pUuid,
+	"sha1":             pSha1,
+	"md5":              pMd5,
+	"datetime":         pDatetime,
+	"ip":               pIp,
+	"duration":         pDuration,
+	"hex":              pHex,
+	"float":            pFloat,
+	"int":              pInt,
+	"bool":             pBool,
+}
+
+var placeholderByPattern = map[int]string{
+	pCurlyBracketed:  "<curly_bracketed>",
+	pSquareBracketed: "<square_bracketed>",
+	pParenthesized:   "<parenthesized>",
+	pDoubleQuoted:    "<double_quoted>",
+	pSingleQuoted:    "<single_quoted>",
+	pGraveQuoted:     "<grave_quoted>",
+	pEmail:           "<email>",
+	pUrl:             "<url>",
+	pHost:            "<host>",
+	pUuid:            "<uuid>",
+	pSha1:            "<sha1>",
+	pMd5:             "<md5>",
+	pDatetime:        "<datetime>",
+	pIp:              "<ip>",
+	pDuration:        "<duration>",
+	pHex:             "<hex>",
+	pFloat:           "<float>",
+	pInt:             "<int>",
+	pBool:            "<bool>",
+}
+
+var normalizeByBytesPatterns = []int{
+	pCurlyBracketed,
+	pSquareBracketed,
+	pParenthesized,
+	pDoubleQuoted,
+	pSingleQuoted,
+	pGraveQuoted,
+}
+
+func onlyNormalizeByBytesPatterns(mask int) bool {
+	if mask <= pNo {
+		return false
+	}
+
+	for p := range placeholderByPattern {
+		if idx := slices.Index(normalizeByBytesPatterns, p); idx != -1 {
+			continue
+		}
+		if mask&p != 0 {
+			return false
+		}
+	}
+	return true
+}
+
 type TokenPattern struct {
 	Placeholder string
 	RE          string
-	Priority    string
+	Priority    string // for custom only
+
+	mask int // for built-in only
 }
 
 type TokenNormalizerParams struct {
-	WithBuiltinPatterns bool
-	Patterns            []TokenPattern
+	BuiltinPatterns string
+	CustomPatterns  []TokenPattern
 }
 
 type tokenNormalizer struct {
-	lexer *lexmachine.Lexer
+	lexer           *lexmachine.Lexer
+	builtinPatterns int
+
+	normalizeByBytes bool
 }
 
 func NewTokenNormalizer(params TokenNormalizerParams) (Normalizer, error) {
-	l := lexmachine.NewLexer()
+	builtinPatterns, err := parseBuiltinPatterns(params.BuiltinPatterns)
+	if err != nil {
+		return nil, err
+	}
 
-	err := initTokens(l, params)
+	n := &tokenNormalizer{
+		builtinPatterns:  builtinPatterns,
+		normalizeByBytes: hasPattern(builtinPatterns, normalizeByBytesPatterns...),
+	}
+
+	// only patterns without regexps
+	if onlyNormalizeByBytesPatterns(builtinPatterns) && len(params.CustomPatterns) == 0 {
+		return n, nil
+	}
+
+	n.lexer = lexmachine.NewLexer()
+
+	err = initTokens(n.lexer, builtinPatterns, params.CustomPatterns)
 	if err != nil {
 		return nil, fmt.Errorf("failed to init tokens: %w", err)
 	}
 
 	defer func() { _ = recover() }()
-	if err = l.Compile(); err != nil {
+	if err = n.lexer.Compile(); err != nil {
 		return nil, fmt.Errorf("failed to compile lexer: %w", err)
 	}
 
-	return &tokenNormalizer{
-		lexer: l,
-	}, nil
+	return n, nil
 }
 
 func (n *tokenNormalizer) Normalize(out, data []byte) []byte {
 	out = out[:0]
 
-	scanner, _ := n.lexer.Scanner(data)
-	prevEnd := 0
-	for tokRaw, err, eos := scanner.Next(); !eos; tokRaw, err, eos = scanner.Next() {
-		if ui, is := err.(*machines.UnconsumedInput); is {
-			scanner.TC = ui.FailTC // skip
-			continue
-		} else if err != nil {
-			out = out[:0]
-			return append(out, data...)
+	var scanner *lexmachine.Scanner
+	if n.normalizeByBytes {
+		out = n.normalizeByTokenizer(out, newTokenizer(n.builtinPatterns, data))
+		if n.lexer == nil {
+			return out
 		}
 
-		tok := tokRaw.(token)
+		scanner, _ = n.lexer.Scanner(out)
 
-		out = append(out, data[prevEnd:tok.begin]...)
-		out = append(out, tok.placeholder...)
-		prevEnd = tok.end
+		// scanner copied buffer, so we can reset it
+		out = out[:0]
+	} else {
+		scanner, _ = n.lexer.Scanner(data)
 	}
-	out = append(out, data[prevEnd:]...)
 
-	return out
+	return n.normalizeByScanner(out, scanner)
 }
 
-type token struct {
-	placeholder string
-	begin       int
-	end         int
+func parseBuiltinPatterns(s string) (int, error) {
+	// single pattern
+	if p, ok := patternById[s]; ok {
+		return p, nil
+	}
+
+	res := 0
+	patterns := strings.Split(s, "|")
+	for _, pId := range patterns {
+		p, ok := patternById[pId]
+		if !ok {
+			return 0, fmt.Errorf("invalid pattern %q", pId)
+		}
+		res += p
+	}
+	return res, nil
 }
 
-func initTokens(lexer *lexmachine.Lexer, params TokenNormalizerParams) error {
+func initTokens(lexer *lexmachine.Lexer,
+	builtinPatterns int, customPatterns []TokenPattern,
+) error {
 	addTokens := func(patterns []TokenPattern) {
 		for _, p := range patterns {
-			lexer.Add([]byte(p.RE), newToken(p.Placeholder))
+			if p.mask == 0 || builtinPatterns&p.mask != 0 {
+				lexer.Add([]byte(p.RE), newToken(p.Placeholder))
+			}
 		}
 	}
 
-	if len(params.Patterns) == 0 {
-		if !params.WithBuiltinPatterns {
+	if len(customPatterns) == 0 {
+		if builtinPatterns == pNo {
 			return errors.New("empty pattern list")
 		}
 		addTokens(builtinTokenPatterns)
 		return nil
 	}
 
-	if !params.WithBuiltinPatterns {
-		addTokens(params.Patterns)
+	if builtinPatterns == pNo {
+		addTokens(customPatterns)
 		return nil
 	}
 
 	lastPatterns := make([]TokenPattern, 0)
 	patterns := make([]TokenPattern, 0)
-	for _, p := range params.Patterns {
+	for _, p := range customPatterns {
 		if p.Priority == patternPriorityFirst {
 			patterns = append(patterns, p)
 		} else {
@@ -112,6 +240,12 @@ func initTokens(lexer *lexmachine.Lexer, params TokenNormalizerParams) error {
 
 	addTokens(patterns)
 	return nil
+}
+
+type token struct {
+	placeholder string
+	begin       int
+	end         int
 }
 
 func newToken(placeholder string) lexmachine.Action {
@@ -130,6 +264,193 @@ func newToken(placeholder string) lexmachine.Action {
 	}
 }
 
+func (n *tokenNormalizer) normalizeByScanner(out []byte, scanner *lexmachine.Scanner) []byte {
+	prevEnd := 0
+	for tokRaw, err, eos := scanner.Next(); !eos; tokRaw, err, eos = scanner.Next() {
+		if ui, is := err.(*machines.UnconsumedInput); is {
+			scanner.TC = ui.FailTC // skip
+			continue
+		} else if err != nil {
+			out = out[:0]
+			return append(out, scanner.Text...)
+		}
+
+		tok := tokRaw.(token)
+
+		out = append(out, scanner.Text[prevEnd:tok.begin]...)
+		out = append(out, tok.placeholder...)
+		prevEnd = tok.end
+	}
+	out = append(out, scanner.Text[prevEnd:]...)
+
+	return out
+}
+
+func (n *tokenNormalizer) normalizeByTokenizer(out []byte, tok *tokenizer) []byte {
+	prevEnd := 0
+	for t, end := tok.nextToken(); !end; t, end = tok.nextToken() {
+		out = append(out, tok.data[prevEnd:t.begin]...)
+		out = append(out, t.placeholder...)
+		prevEnd = t.end
+	}
+	out = append(out, tok.data[prevEnd:]...)
+
+	return out
+}
+
+func hasPattern(patterns int, masks ...int) bool {
+	if patterns == pAll {
+		return true
+	}
+	for _, m := range masks {
+		if patterns&m != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+type tokenizer struct {
+	patterns int
+	data     []byte
+	pos      int
+
+	curPattern   int
+	counter      int
+	startPattern int
+}
+
+func newTokenizer(patterns int, data []byte) *tokenizer {
+	return &tokenizer{
+		patterns: patterns,
+		data:     data,
+	}
+}
+
+func (t *tokenizer) nextToken() (token, bool) {
+	t.curPattern = 0
+	t.counter = 0
+	t.startPattern = 0
+
+	for i := t.pos; i < len(t.data); i++ {
+		switch {
+		case t.data[i] == '{' && hasPattern(t.patterns, pCurlyBracketed):
+			t.processOpenBracket(pCurlyBracketed, i)
+		case t.data[i] == '}' && hasPattern(t.patterns, pCurlyBracketed):
+			if t, ok := t.processCloseBracket(pCurlyBracketed, i); ok {
+				return t, false
+			}
+		case t.data[i] == '[' && hasPattern(t.patterns, pSquareBracketed):
+			t.processOpenBracket(pSquareBracketed, i)
+		case t.data[i] == ']' && hasPattern(t.patterns, pSquareBracketed):
+			if t, ok := t.processCloseBracket(pSquareBracketed, i); ok {
+				return t, false
+			}
+		case t.data[i] == '(' && hasPattern(t.patterns, pParenthesized):
+			t.processOpenBracket(pParenthesized, i)
+		case t.data[i] == ')' && hasPattern(t.patterns, pParenthesized):
+			if t, ok := t.processCloseBracket(pParenthesized, i); ok {
+				return t, false
+			}
+		case t.data[i] == '"' && hasPattern(t.patterns, pDoubleQuoted):
+			t, shift, ok := t.processQuotes('"', pDoubleQuoted, i)
+			if ok {
+				return t, false
+			}
+			i += shift
+		case t.data[i] == '\'' && hasPattern(t.patterns, pSingleQuoted):
+			t, shift, ok := t.processQuotes('\'', pSingleQuoted, i)
+			if ok {
+				return t, false
+			}
+			i += shift
+		case t.data[i] == '`' && hasPattern(t.patterns, pGraveQuoted):
+			t, shift, ok := t.processQuotes('`', pGraveQuoted, i)
+			if ok {
+				return t, false
+			}
+			i += shift
+		}
+	}
+
+	// last partial token for possible cropped data
+	if t.curPattern != 0 {
+		t.pos = len(t.data)
+		return token{
+			placeholder: placeholderByPattern[t.curPattern],
+			begin:       t.startPattern,
+			end:         t.pos,
+		}, false
+	}
+
+	return token{}, true
+}
+
+func (t *tokenizer) processOpenBracket(pattern int, pos int) {
+	if t.curPattern == 0 {
+		t.curPattern = pattern
+		t.counter = 1
+		t.startPattern = pos
+	} else if t.curPattern == pattern {
+		t.counter++
+	}
+}
+
+func (t *tokenizer) processCloseBracket(pattern int, pos int) (token, bool) {
+	if t.curPattern != pattern {
+		return token{}, false
+	}
+
+	t.counter--
+	if t.counter > 0 {
+		return token{}, false
+	}
+
+	t.pos = pos + 1
+	return token{
+		placeholder: placeholderByPattern[pattern],
+		begin:       t.startPattern,
+		end:         t.pos,
+	}, true
+}
+
+func (t *tokenizer) processQuotes(c byte, pattern int, pos int) (token, int, bool) {
+	if t.curPattern == 0 {
+		t.curPattern = pattern
+		t.counter = 1
+		t.startPattern = pos
+
+		// multiple quotes in a row
+		for i := pos + 1; i < len(t.data) && t.data[i] == c; i++ {
+			t.counter++
+		}
+
+		return token{}, t.counter - 1, false
+	} else if t.curPattern == pattern {
+		// skip escaped
+		if pos > 0 && t.data[pos-1] == '\\' {
+			return token{}, 0, false
+		}
+
+		tmp := t.counter - 1
+		// multiple quotes in a row
+		for i := pos + 1; i < len(t.data) && t.data[i] == c; i++ {
+			tmp--
+		}
+		if tmp > 0 {
+			return token{}, t.counter - tmp - 1, false
+		}
+
+		t.pos = pos + t.counter
+		return token{
+			placeholder: placeholderByPattern[pattern],
+			begin:       t.startPattern,
+			end:         t.pos,
+		}, 0, true
+	}
+	return token{}, 0, false
+}
+
 func isWord(c byte) bool {
 	return '0' <= c && c <= '9' ||
 		'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' ||
@@ -140,23 +461,29 @@ func isWord(c byte) bool {
 // so we use [strings.Repeat] instead
 var builtinTokenPatterns = []TokenPattern{
 	{
-		Placeholder: "<email>",
-		RE:          `[0-9a-zA-Z_\.\-]+@[0-9a-zA-Z_\-]+(\.[0-9a-zA-Z_\-]+)*`,
+		Placeholder: placeholderByPattern[pEmail],
+		RE:          `[0-9a-zA-Z_\.\-\*]+@[0-9a-zA-Z_\-]+(\.[0-9a-zA-Z_\-]+)*`,
+
+		mask: pEmail,
 	},
 	{
-		Placeholder: "<url>",
+		Placeholder: placeholderByPattern[pUrl],
 		RE:          `(wss?|https?|ftp)://[0-9a-zA-Z_\.\-@:%\+~#=\?/]+`,
+
+		mask: pUrl,
 	},
 	{
-		Placeholder: "<host>",
+		Placeholder: placeholderByPattern[pHost],
 		RE: fmt.Sprintf(`([0-9a-zA-Z_\-]+\.)+(%s|%s)`,
 			// top 100 TLDs
 			`COM|NET|ORG|JP|DE|UK|FR|BR|IT|RU|ES|ME|GOV|PL|CA|AU|CN|CO|IN|NL|EDU|INFO|EU|CH|ID|AT|KR|CZ|MX|BE|TV|SE|TR|TW|AL|UA|IR|VN|CL|SK|LY|CC|TO|NO|FI|US|PT|DK|AR|HU|TK|GR|IL|NEWS|RO|MY|BIZ|IE|ZA|NZ|SG|EE|TH|IO|XYZ|PE|BG|HK|RS|LT|LINK|PH|CLUB|SI|SITE|MOBI|BY|CAT|WIKI|LA|GA|XXX|CF|HR|NG|JOBS|ONLINE|KZ|UG|GQ|AE|IS|LV|PRO|FM|TIPS|MS|SA|APP`,
 			`com|net|org|jp|de|uk|fr|br|it|ru|es|me|gov|pl|ca|au|cn|co|in|nl|edu|info|eu|ch|id|at|kr|cz|mx|be|tv|se|tr|tw|al|ua|ir|vn|cl|sk|ly|cc|to|no|fi|us|pt|dk|ar|hu|tk|gr|il|news|ro|my|biz|ie|za|nz|sg|ee|th|io|xyz|pe|bg|hk|rs|lt|link|ph|club|si|site|mobi|by|cat|wiki|la|ga|xxx|cf|hr|ng|jobs|online|kz|ug|gq|ae|is|lv|pro|fm|tips|ms|sa|app`,
 		),
+
+		mask: pHost,
 	},
 	{
-		Placeholder: "<uuid>",
+		Placeholder: placeholderByPattern[pUuid],
 		RE: fmt.Sprintf(`%s-%s-%s-%s-%s`,
 			strings.Repeat(`[0-9a-fA-F]`, 8),
 			strings.Repeat(`[0-9a-fA-F]`, 4),
@@ -164,47 +491,67 @@ var builtinTokenPatterns = []TokenPattern{
 			strings.Repeat(`[0-9a-fA-F]`, 4),
 			strings.Repeat(`[0-9a-fA-F]`, 12),
 		),
+
+		mask: pUuid,
 	},
 	{
-		Placeholder: "<sha1>",
+		Placeholder: placeholderByPattern[pSha1],
 		RE:          strings.Repeat(`[0-9a-fA-F]`, 40),
+
+		mask: pSha1,
 	},
 	{
-		Placeholder: "<md5>",
+		Placeholder: placeholderByPattern[pMd5],
 		RE:          strings.Repeat(`[0-9a-fA-F]`, 32),
+
+		mask: pMd5,
 	},
 	{
 		// RFC3339, RFC3339Nano, DateTime, DateOnly, TimeOnly
-		Placeholder: "<datetime>",
+		Placeholder: placeholderByPattern[pDatetime],
 		RE: fmt.Sprintf(`(%s)|(%s)|(%s)`,
 			`\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(Z|[\+\-]\d\d:\d\d)`,
 			`\d\d:\d\d:\d\d`,
 			`\d\d\d\d-\d\d-\d\d( \d\d:\d\d:\d\d)?`,
 		),
+
+		mask: pDatetime,
 	},
 	{
 		// IPv4 only
-		Placeholder: "<ip>",
+		Placeholder: placeholderByPattern[pIp],
 		RE:          strings.TrimSuffix(strings.Repeat(`(25[0-5]|(2[0-4]|1?[0-9])?[0-9])\.`, 4), `\.`),
+
+		mask: pIp,
 	},
 	{
-		Placeholder: "<duration>",
+		Placeholder: placeholderByPattern[pDuration],
 		RE:          `-?((\d+|\d+\.\d+)(ns|us|µs|ms|s|m|h|d|w))+`,
+
+		mask: pDuration,
 	},
 	{
-		Placeholder: "<hex>",
+		Placeholder: placeholderByPattern[pHex],
 		RE:          `0[xX][0-9a-fA-F]+`,
+
+		mask: pHex,
 	},
 	{
-		Placeholder: "<float>",
+		Placeholder: placeholderByPattern[pFloat],
 		RE:          `-?\d+\.\d+`,
+
+		mask: pFloat,
 	},
 	{
-		Placeholder: "<int>",
+		Placeholder: placeholderByPattern[pInt],
 		RE:          `-?\d+`,
+
+		mask: pInt,
 	},
 	{
-		Placeholder: "<bool>",
+		Placeholder: placeholderByPattern[pBool],
 		RE:          `[Tt][Rr][Uu][Ee]|[Ff][Aa][Ll][Ss][Ee]`,
+
+		mask: pBool,
 	},
 }
