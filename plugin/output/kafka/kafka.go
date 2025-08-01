29739225aed9f81cbbe4370c33f500d4c7ad6d99
Timofey Gukov <90568801+timggggggg@users.noreply.github.com>
2025-08-01 13:37:29 +0300

    ForceMetadataRefresh on NotLeaderForPartition error (#888)

--- plugin/output/kafka/kafka.go
+++ plugin/output/kafka/kafka.go
@@ -116,6 +116,12 @@ type Config struct {
 	// >
 	// > Retries of insertion. If File.d cannot insert for this number of attempts,
 	// > File.d will fall with non-zero exit code or skip message (see fatal_on_failed_insert).
+	// >
+	// > There are situations when one of the brokers is disconnected and the client does not have time to
+	// > update the metadata before all the remaining retries are finished. To avoid this situation,
+	// > the client.ForceMetadataRefresh() function is used for some ProduceSync errors:
+	// > - kerr.LeaderNotAvailable - There is no leader for this topic-partition as we are in the middle of a leadership election.
+	// > - kerr.NotLeaderForPartition - This server is not the leader for that topic-partition.
 	Retry int `json:"retry" default:"10"` // *
 
 	// > @3@4@5@6
@@ -326,7 +332,7 @@ func (p *Plugin) out(workerData *pipeline.WorkerData, batch *pipeline.Batch) err
 	})
 
 	if err := p.client.ProduceSync(context.Background(), data.messages[:i]...).FirstErr(); err != nil {
-		if errors.Is(err, kerr.LeaderNotAvailable) {
+		if errors.Is(err, kerr.LeaderNotAvailable) || errors.Is(err, kerr.NotLeaderForPartition) {
 			p.client.ForceMetadataRefresh()
 		}
 		p.logger.Errorf("can't write batch: %v", err)
