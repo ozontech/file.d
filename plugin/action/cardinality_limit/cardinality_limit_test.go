e80684fe861204a5916261ad30c0a2926b0b5cf5
Dmitry Romanov <dmitry.romanov85@protonmail.com>
2025-08-21 18:46:10 +0700

    add cardinality limit tests

--- plugin/action/cardinality_limit/cardinality_limit_test.go
+++ plugin/action/cardinality_limit/cardinality_limit_test.go
@@ -1,9 +1,17 @@
 package discard
 
 import (
+	"fmt"
 	"sort"
 	"strings"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/ozontech/file.d/cfg"
+	"github.com/ozontech/file.d/pipeline"
+	"github.com/ozontech/file.d/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMapToStringSorted(t *testing.T) {
@@ -46,4 +54,147 @@ func TestMapToStringSorted(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCardinalityLimitDiscard(t *testing.T) {
+	limit := 10
+	config := &Config{
+		KeyFields: []cfg.FieldSelector{"host"},
+		Fields:    []cfg.FieldSelector{"i"},
+		Limit:     limit,
+		Action:    actionDiscard,
+		TTL_:      1 * time.Hour,
+	}
+
+	p, input, output := test.NewPipelineMock(test.NewActionPluginStaticInfo(factory, config, pipeline.MatchModeAnd, nil, false))
+
+	inEventsCnt := 0
+	inWg := sync.WaitGroup{}
+	genEventsCnt := limit + 10
+	inWg.Add(genEventsCnt)
+
+	input.SetInFn(func() {
+		defer inWg.Done()
+		inEventsCnt++
+	})
+
+	outEventsCnt := 0
+	output.SetOutFn(func(e *pipeline.Event) {
+		outEventsCnt++
+	})
+
+	for i := 0; i < genEventsCnt; i++ {
+		json := fmt.Sprintf(`{"host":"localhost","i":"%d"}`, i)
+		input.In(10, "test", test.NewOffset(0), []byte(json))
+	}
+	inWg.Wait()
+	time.Sleep(100 * time.Millisecond)
+
+	p.Stop()
+	assert.Equal(t, inEventsCnt, genEventsCnt, "wrong in events count")
+	assert.Equal(t, limit, outEventsCnt, "wrong out events count")
+}
+
+func TestCardinalityLimitRemoveFields(t *testing.T) {
+	limit := 10
+	config := &Config{
+		KeyFields: []cfg.FieldSelector{"host"},
+		Fields:    []cfg.FieldSelector{"i"},
+		Limit:     limit,
+		Action:    actionRemoveFields,
+		TTL_:      1 * time.Hour,
+	}
+
+	p, input, output := test.NewPipelineMock(test.NewActionPluginStaticInfo(factory, config, pipeline.MatchModeAnd, nil, false))
+
+	inEventsCnt := 0
+	inWg := sync.WaitGroup{}
+	genEventsCnt := limit + 10
+	inWg.Add(genEventsCnt)
+
+	input.SetInFn(func() {
+		defer inWg.Done()
+		inEventsCnt++
+	})
+
+	outEventsCnt := 0
+	outWg := sync.WaitGroup{}
+	outWg.Add(genEventsCnt)
+	output.SetOutFn(func(e *pipeline.Event) {
+		defer outWg.Done()
+
+		// check exists field
+		value := pipeline.CloneString(e.Root.Dig(string(config.Fields[0])).AsString())
+
+		if value != "" {
+			outEventsCnt++
+		}
+	})
+
+	for i := 0; i < genEventsCnt; i++ {
+		json := fmt.Sprintf(`{"host":"localhost","i":"%d"}`, i)
+		input.In(10, "test", test.NewOffset(0), []byte(json))
+	}
+	inWg.Wait()
+	outWg.Wait()
+
+	p.Stop()
+	assert.Equal(t, inEventsCnt, genEventsCnt, "wrong in events count")
+	assert.Equal(t, limit, outEventsCnt, "wrong out events count")
+}
+
+func TestCardinalityLimitDiscardIfNoSetKeyFields(t *testing.T) {
+	limit := 10
+	config := &Config{
+		KeyFields: []cfg.FieldSelector{},
+		Fields:    []cfg.FieldSelector{"i"},
+		Limit:     limit,
+		Action:    actionDiscard,
+		TTL_:      1 * time.Hour,
+	}
+
+	p, input, output := test.NewPipelineMock(test.NewActionPluginStaticInfo(factory, config, pipeline.MatchModeAnd, nil, false))
+
+	inEventsCnt := 0
+	inWg := sync.WaitGroup{}
+	genEventsCnt := limit + 10
+	inWg.Add(genEventsCnt)
+
+	input.SetInFn(func() {
+		defer inWg.Done()
+		inEventsCnt++
+	})
+
+	outEventsCnt := 0
+	output.SetOutFn(func(e *pipeline.Event) {
+		outEventsCnt++
+	})
+
+	for i := 0; i < genEventsCnt; i++ {
+		json := fmt.Sprintf(`{"host":"localhost%d","i":"%d"}`, i, i)
+		input.In(10, "test", test.NewOffset(0), []byte(json))
+	}
+	inWg.Wait()
+	time.Sleep(100 * time.Millisecond)
+
+	p.Stop()
+	assert.Equal(t, inEventsCnt, genEventsCnt, "wrong in events count")
+	assert.Equal(t, limit, outEventsCnt, "wrong out events count")
+}
+
+func TestSetAndCountPrefix(t *testing.T) {
+	cache, _ := NewCache(time.Minute)
+
+	cacheKey := map[string]string{
+		"host": "localhost",
+	}
+	cacheValue := map[string]string{
+		"i": "0",
+	}
+	key := mapToKey(cacheKey)
+	value := mapToStringSorted(cacheKey, cacheValue)
+	cache.Set(value)
+
+	keysCount := cache.CountPrefix(key)
+	assert.Equal(t, 1, keysCount, "wrong in events count")
 }
