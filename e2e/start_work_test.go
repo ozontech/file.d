e4c0dcd76e512ab5f6559bc5b8390fbe2a9691a5
Sergey Lazarenko <sergey.lazarenko.0241@mail.ru>
2026-05-29 21:37:42 +0300

    fix

--- e2e/start_work_test.go
+++ e2e/start_work_test.go
@@ -166,7 +166,7 @@ func TestE2EStabilityWorkCase(t *testing.T) {
 		{
 			name: "file_elasticsearch",
 			e2eTest: &file_elasticsearch.Config{
-				Count:     50,
+				Count:     10,
 				Pipeline:  "test-ingest-pipeline",
 				Endpoints: []string{"http://localhost:19200"},
 				Username:  "elastic",
@@ -177,7 +177,7 @@ func TestE2EStabilityWorkCase(t *testing.T) {
 		{
 			name: "file_elasticsearch_cb",
 			e2eTest: &file_elasticsearch.Config{
-				Count:     50,
+				Count:     10,
 				Pipeline:  "test-ingest-pipeline",
 				Endpoints: []string{"http://localhost:19200", "http://localhost:19201"},
 				Username:  "elastic",
