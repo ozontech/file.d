755ece3dd27c22dbb00411faf815bfa8c5114ca6
Yaroslav Kirillov <kirillov.y6@gmail.com>
2026-08-03 14:49:30 +0500

    fix doc

--- plugin/input/kafka/kafka.go
+++ plugin/input/kafka/kafka.go
@@ -197,13 +197,13 @@ type Config struct {
 	// >
 	// > `OAuthConfig` params:
 	// > * **`token`** *`string`* - static token
-	// > ---
+	// >
 	// > * **`client_id`** *`string`* - client ID
 	// > * **`client_secret`** *`string`* - client secret
 	// > * **`token_url`** *`string`* - resource server's token endpoint URL
 	// > * **`scopes`** *`[]string`* - optional requested permissions
 	// > * **`auth_style`** *`string`* *`default=params`* *`options=params|header`* - specifies how the endpoint wants the client ID & client secret sent
-	// > ---
+	// >
 	// > * **`tls`** *`TLSConfig`* - tls config
 	// >
 	// > `TLSConfig` params:
