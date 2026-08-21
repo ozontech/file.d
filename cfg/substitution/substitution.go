c7a25b9ff4f0e2c2e45b97759fc3be0f0608178d
Oleg Don <31502412+HeadHunter483@users.noreply.github.com>
2026-08-21 20:42:55 +0500

    Fix parsing pipe in substitution

--- cfg/substitution/substitution.go
+++ cfg/substitution/substitution.go
@@ -108,8 +108,9 @@ func ParseSubstitution(substitution string, filtersBuf []byte, logger *zap.Logge
 
 			var filterOps []FieldFilter
 			selectorEnd := end
-			pipe := indexRuneInExpr(substitution, '|', true, false)
+			pipe := indexRuneInExpr(substitution[pos+2:end], '|', true, false)
 			if pipe != -1 {
+				pipe += pos + 2 // correct pipe pos in substitution
 				selectorEnd = pipe
 				filterOps, err = parseFilterOps(substitution, pipe, end, filtersBuf, logger)
 				if err != nil {
