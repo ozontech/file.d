e18976517f52c99dd8a58339419f9f6c27364aee
Yaroslav Kirillov <kirillov.y6@gmail.com>
2026-09-21 15:12:49 +0500

    fix comment

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -439,14 +439,6 @@ func (p *Pipeline) In(sourceID SourceID, sourceName string, offsets Offsets, byt
 		}
 	}
 
-	// Skip IsSpam for partial logs is necessary to avoid the case
-	// when some parts of a large event have got into the ban,
-	// thereby cutting off a piece of the event.
-	// This is only possible if the event was written in CRI format and has Partial status.
-	// For other encoding formats this is not relevant as they always come in full.
-	// The event is Partial if it is larger than the driver configuration.
-	// For example, for containerd this setting is called max_container_log_line_size
-	// https://github.com/containerd/containerd/blob/f7f2be732159a411eae46b78bfdb479b133a823b/pkg/cri/config/config.go#L263-L266
 	if p.settings.Antispam.Threshold >= 0 {
 		streamOffset := offsets.ByStream(string(row.Stream))
 		currentOffset := offsets.current
